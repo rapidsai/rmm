7d8993089d554245125cd6022ff6e5edc8723046
gpuCI <38199262+GPUtester@users.noreply.github.com>
2019-03-18 02:38:16 -0700

    REL v0.6.0 release

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='librmm_cffi',
-      version="0.5.0",
+      version="0.6.0",
       packages=["librmm_cffi"],
       setup_requires=["cffi>=1.0.0"],
       cffi_modules=["librmm_cffi/librmm_build.py:ffibuilder"],
