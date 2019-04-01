5d2010bec4a934608b029bffe70f54ec9275e20f
Keith Kraus <kkraus@nvidia.com>
2019-04-01 11:48:31 -0400

    Forgot to change cudf to rmm

--- ci/cpu/rmm/build_rmm.sh
+++ ci/cpu/rmm/build_rmm.sh
@@ -4,5 +4,5 @@ if [ "$BUILD_RMM" == "1" ]; then
   echo "Building rmm"
   export RMM_BUILD_NO_GPU_TEST=1
 
-  conda build conda/recipes/cudf -c rapidsai -c rapidsai-nightly -c nvidia -c conda-forge --python=$PYTHON
+  conda build conda/recipes/rmm -c rapidsai -c rapidsai-nightly -c nvidia -c conda-forge --python=$PYTHON
 fi
