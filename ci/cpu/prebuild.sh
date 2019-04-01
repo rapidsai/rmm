71cf626bd48f6016fe07eca521da414b1b5257e9
Keith Kraus <kkraus@nvidia.com>
2019-04-01 11:55:49 -0400

    Always build both temporarily

--- ci/cpu/prebuild.sh
+++ ci/cpu/prebuild.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 #Build rmm once per PYTHON
-if [[ "$CUDA" == "9.2" ]]; then
-    export BUILD_RMM=1
-else
-    export BUILD_RMM=0
-fi
+export BUILD_RMM=1
+# if [[ "$CUDA" == "9.2" ]]; then
+#     export BUILD_RMM=1
+# else
+#     export BUILD_RMM=0
+# fi
 
 #Build librmm once per CUDA
-if [[ "$PYTHON" == "3.6" ]]; then
-    export BUILD_LIBRMM=1
-else
-    export BUILD_LIBRMM=0
-fi
+export BUILD_LIBRMM=1
+# if [[ "$PYTHON" == "3.6" ]]; then
+#     export BUILD_LIBRMM=1
+# else
+#     export BUILD_LIBRMM=0
+# fi
