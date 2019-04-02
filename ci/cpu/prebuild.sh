437dcfe397367e1a18ed93db159e127b5e757e40
Keith Kraus <kkraus@nvidia.com>
2019-04-02 12:04:37 -0400

    Uncomment the prebuild setup, add python version to rmm recipe

--- ci/cpu/prebuild.sh
+++ ci/cpu/prebuild.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
 #Build rmm once per PYTHON
-export BUILD_RMM=1
-# if [[ "$CUDA" == "9.2" ]]; then
-#     export BUILD_RMM=1
-# else
-#     export BUILD_RMM=0
-# fi
+if [[ "$CUDA" == "9.2" ]]; then
+    export BUILD_RMM=1
+else
+    export BUILD_RMM=0
+fi
 
 #Build librmm once per CUDA
-export BUILD_LIBRMM=1
-# if [[ "$PYTHON" == "3.6" ]]; then
-#     export BUILD_LIBRMM=1
-# else
-#     export BUILD_LIBRMM=0
-# fi
+if [[ "$PYTHON" == "3.6" ]]; then
+    export BUILD_LIBRMM=1
+else
+    export BUILD_LIBRMM=0
+fi
