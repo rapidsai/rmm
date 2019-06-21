f92be1a7edc72193435efc5590e815a1ff4c698a
Markku Luukkainen <mluukkainen@nvidia.com>
2019-06-21 08:35:23 -0700

    Add build script

--- conda/recipes/rmm/build.sh
+++ conda/recipes/rmm/build.sh
@@ -1,13 +1,6 @@
-CMAKE_COMMON_VARIABLES=" -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release"
+# Copyright (c) 2018-2019, NVIDIA CORPORATION.
 
-# Cleanup local git
-git clean -xdff
-# Use CMake-based build procedure
-mkdir build
-cd build
-# configure
-cmake $CMAKE_COMMON_VARIABLES ..
-# build
-make -j${PARALLEL_LEVEL} VERBOSE=1 rmm_python_cffi
-# install
-make -j${PARALLEL_LEVEL} VERBOSE=1 rmm_install_python
+# Script assumes the script is executed from the root of the repo directory
+printenv
+git clean -xdf
+./build.sh -v clean rmm
