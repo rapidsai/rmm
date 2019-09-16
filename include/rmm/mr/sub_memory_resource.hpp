fad9fab3de4032d14ed0fda7372b5259c840adfe
Mark Harris <mharris@nvidia.com>
2019-09-15 18:11:36 -0700

    Initial pool allocation, block merging, bug fixes.

--- include/rmm/mr/sub_memory_resource.hpp
+++ include/rmm/mr/sub_memory_resource.hpp
@@ -66,7 +66,20 @@ class sub_memory_resource final : public device_memory_resource {
                                std::size_t maximum_pool_size = default_maximum_size)
     : heap_resource{rmm::mr::get_default_resource()}
   {
-    // TODO allocate initial block
+    if (initial_pool_size == default_initial_size)  {
+      int device{0};
+      cudaGetDevice(&device);
+      int memsize{0};
+      cudaDeviceProp props;
+      cudaGetDeviceProperties(&props, device);
+      initial_pool_size = props.totalGlobalMem / 2;
+
+      if (maximum_pool_size == default_maximum_size)
+        maximum_pool_size = props.totalGlobalMem;
+    }
+
+    // Allocate initial block
+    no_sync_blocks.insert(block_from_heap(initial_pool_size, 0));
 
     // TODO device handling?
 
@@ -76,6 +89,7 @@ class sub_memory_resource final : public device_memory_resource {
   }
 
   ~sub_memory_resource() {
+    free_all();
   }
 
   bool supports_streams() const noexcept override { return true; }
@@ -84,35 +98,42 @@ class sub_memory_resource final : public device_memory_resource {
 
   struct block
   {
-    intptr_t ptr;
+    char* ptr;
     size_t size;
 
-    bool operator==(block const* rhs) const {
-      return this->ptr == rhs->ptr && this->size == rhs->size;
+    bool operator<(const block& rhs) const { 
+      if (size < rhs.size) return ptr < rhs.ptr;
+      return false;
     }
-    bool operator<(const block *rhs) const { return this->size < rhs->size; }
   };
 
-  using block_set = std::set<block*>;
+  using block_set = std::set<block>;
 
-  inline block* next_larger_block(block_set &blocks, size_t size)
+  inline block next_larger_block(block_set &blocks, size_t size)
   {
     block dummy{0, size};
-    auto iter = blocks.lower_bound(&dummy);
+    auto iter = blocks.lower_bound(dummy);
+
+    if (iter != blocks.end())
+    {
+      block found = *iter;
+      blocks.erase(iter);
+      return found;
+    }
     
-    return iter != blocks.end() ? *iter : nullptr;
+    return dummy;
   }
 
-  inline block* available_larger_block(size_t size, cudaStream_t stream)
+  inline block available_larger_block(size_t size, cudaStream_t stream)
   {
     // Try to find a larger block that doesn't require syncing
-    block* b = next_larger_block(no_sync_blocks, size);
+    block b = next_larger_block(no_sync_blocks, size);
 
     // Try to find a larger block in a different stream
-    if (!b) {
+    if (b.ptr == nullptr) {
       for (auto s : sync_blocks) {
         if (s.first != stream) b = next_larger_block(s.second, size);
-        if (b) {
+        if (b.ptr != nullptr) {
           // synchronize the stream associated with this list
           cudaError_t result = cudaStreamSynchronize(s.first);
           
@@ -126,60 +147,101 @@ class sub_memory_resource final : public device_memory_resource {
 
           // remove this stream from the freelist
           sync_blocks.erase(stream);
-          break;
+          return b;
         }
       }
+
+      // no larger blocks waiting on other streams, so create one
+      b = block_from_heap(size, stream);
     }
 
-    // no larger blocks available, create one
-    b = block_from_heap(size, stream);
-
+    
     return b;
   }
 
-  inline block* split_block(block *b, size_t size, cudaStream_t stream)
+  inline block split_block(block b, size_t size)
   {
-    if (b->size > size)
+    if (b.size > size)
     {
-      block *rest = new block{static_cast<intptr_t>(b->ptr+size), b->size - size};
-      b->size = size;
+      block rest{b.ptr + size, b.size - size};
       no_sync_blocks.insert(rest);
+      b.size = size;
     }
 
     return b;
   }
 
-  inline intptr_t allocate_from_block(block *b, size_t size, cudaStream_t stream)
+  inline block merge_blocks(const block& a, const block& b)
   {
-    if (b == nullptr) throw std::bad_alloc{};
-    b = split_block(b, size, stream);
-    allocated_blocks[b->ptr] = b;
-    return b->ptr;
+    if (a.ptr + a.size != b.ptr)
+      throw std::logic_error("Invalid block merge");
+    
+    return block{a.ptr, a.size + b.size};
   }
 
-  inline void find_and_deallocate_block(void *p, size_t size, cudaStream_t stream)
+  inline void* allocate_from_block(const block& b, size_t size)
+  {
+    if (b.ptr == nullptr) throw std::bad_alloc{};
+    block split = split_block(b, size);
+    allocated_blocks[split.ptr] = split;
+    return reinterpret_cast<void*>(split.ptr);
+  }
+
+  // check if next / prev are in blocks and merge if so
+  inline void free_block(const block& b, block_set& blocks) {
+    block_set::iterator prev{blocks.end()}, next{blocks.end()};
+
+    // TODO this is a linear search since the set is ordered by size
+    // would be nice to have a faster way
+    for (auto iter = blocks.begin(); iter != blocks.end(); ++iter) {
+      if (iter->ptr + iter->size == b.ptr) prev = iter;
+      else if (b.ptr + b.size == iter->ptr) { next = iter; break; }
+    }
+
+    block merged = b;
+    if (prev != blocks.end()) {
+      merged = merge_blocks(*prev, merged);  
+      blocks.erase(prev);
+    }
+
+    if (next != blocks.end()) {
+      merged = merge_blocks(merged, *next);
+      blocks.erase(next);
+    }
+
+    blocks.insert(merged);
+  }
+
+  inline void find_and_free_block(void *p, size_t size, cudaStream_t stream)
   {
     if (p == nullptr) return;
 
-    auto i = allocated_blocks.find(reinterpret_cast<intptr_t>(p));
-    block *b = nullptr;
+    auto i = allocated_blocks.find(reinterpret_cast<char*>(p));
+    block b{};
     if (i != allocated_blocks.end()) { // found
-      b = std::move(i->second);
-      assert(b->size == detail::round_up_safe(size, allocation_alignment));
+      b = i->second;
+      assert(b.size == detail::round_up_safe(size, allocation_alignment));
       allocated_blocks.erase(i);
-      sync_blocks[stream].insert(b);
+      free_block(b, sync_blocks[stream]);
     }
     else {
       throw std::runtime_error("Pointer not allocated by this resource.");
     }
   }
 
-  inline block* block_from_heap(size_t size, cudaStream_t stream)
+  inline block block_from_heap(size_t size, cudaStream_t stream)
   {
     void* p = heap_resource->allocate(size, stream);
-    if (p != nullptr)
-      return new block{reinterpret_cast<intptr_t>(p), size};
-    else return nullptr;
+    if (p == nullptr) throw std::bad_alloc{};
+    block b{reinterpret_cast<char*>(p), size};
+    heap_blocks[b.ptr] = b;
+    return b;
+  }
+
+  inline void free_all()
+  {
+    for (auto b : heap_blocks) heap_resource->deallocate(b.first, b.second.size);
+    heap_blocks.clear();
   }
 
   /**---------------------------------------------------------------------------*
@@ -195,8 +257,8 @@ class sub_memory_resource final : public device_memory_resource {
   void* do_allocate(std::size_t bytes, cudaStream_t stream) override {
     if (bytes <= 0) return nullptr;
     bytes = detail::round_up_safe(bytes, allocation_alignment);
-    block *b = available_larger_block(bytes, stream);
-    return reinterpret_cast<void*>(allocate_from_block(b, bytes, stream));
+    block b = available_larger_block(bytes, stream);
+    return allocate_from_block(b, bytes);
   }
 
   /**---------------------------------------------------------------------------*
@@ -207,7 +269,7 @@ class sub_memory_resource final : public device_memory_resource {
    * @param p Pointer to be deallocated
    *---------------------------------------------------------------------------**/
   void do_deallocate(void* p, std::size_t bytes, cudaStream_t stream) override {
-    find_and_deallocate_block(p, bytes, stream);
+    find_and_free_block(p, bytes, stream);
   }
 
   /**---------------------------------------------------------------------------*
@@ -242,7 +304,10 @@ class sub_memory_resource final : public device_memory_resource {
   std::map<cudaStream_t, block_set> sync_blocks;
 
   // allocated_blocks: map of allocated [ptr, block] pairs
-  std::map<std::intptr_t, block*> allocated_blocks;
+  std::map<char*, block> allocated_blocks;
+
+  // blocks allocated from heap: so they can be easily freed
+  std::map<char*, block> heap_blocks;
 };
 
 }  // namespace mr
