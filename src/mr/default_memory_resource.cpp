e521e95b45cb593a0f884d77aa634281660e42f8
Mark Harris <mharris@nvidia.com>
2019-10-07 23:57:29 -0700

    Remove all RMM singletons except for the default resource, fix rmmInitialize/rmmFinalize

--- src/mr/default_memory_resource.cpp
+++ src/mr/default_memory_resource.cpp
@@ -25,49 +25,20 @@
 namespace rmm {
 namespace mr {
 
-namespace detail{
-device_memory_resource* initial_resource() {
-  return cuda_resource();
-}
-
-device_memory_resource* cuda_resource() {
-  static cuda_memory_resource resource{};
-  return &resource;
-}
-
-device_memory_resource* pool_resource(std::size_t pool_size){
-   static cnmem_memory_resource mr{pool_size};
-   return &mr;
-}
-
-device_memory_resource* managed_pool_resource(std::size_t pool_size){
-   static cnmem_managed_memory_resource mr{pool_size};
-   return &mr;
-}
-
-device_memory_resource* managed_resource(){
-   static managed_memory_resource mr{};
-   return &mr;
-}
-} //namespace detail
 namespace {
-
-
-
-
-
 // Use an atomic to guarantee thread safety
 std::atomic<device_memory_resource*>& get_default() {
-  static std::atomic<device_memory_resource*> res{detail::initial_resource()};
+  static std::atomic<device_memory_resource*> res{new cuda_memory_resource{}};
   return res;
 }
-}  // namespace
+}  // namespace anonymous
 
 device_memory_resource* get_default_resource() { return get_default().load(); }
 
 device_memory_resource* set_default_resource(
-    device_memory_resource* new_resource) {
-  new_resource = (new_resource == nullptr) ? detail::initial_resource() : new_resource;
+  device_memory_resource* new_resource) {
+  new_resource = (new_resource == nullptr) ?
+    new cuda_memory_resource() : new_resource;
   return get_default().exchange(new_resource);
 }
 }  // namespace mr
