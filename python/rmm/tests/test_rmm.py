b36936c377035670514ecc2b5da6e997a6daee6a
Mads R. B. Kristensen <madsbk@gmail.com>
2019-12-09 13:03:45 +0100

    fixed test of rmm_cupy_allocator()

--- python/rmm/tests/test_rmm.py
+++ python/rmm/tests/test_rmm.py
@@ -83,15 +83,15 @@ def test_rmm_csv_log(dtype, nelem):
 def test_rmm_cupy_allocator():
     cupy = pytest.importorskip("cupy")
 
-    mem = rmm.rmm_cupy_allocator(42)
-    assert mem.size == 42
-    assert mem.ptr != 0
-    assert mem.rmm_array is not None
+    m = rmm.rmm_cupy_allocator(42)
+    assert m.mem.size == 42
+    assert m.mem.ptr != 0
+    assert m.mem.rmm_array is not None
 
-    mem = rmm.rmm_cupy_allocator(0)
-    assert mem.size == 0
-    assert mem.ptr == 0
-    assert mem.rmm_array is None
+    m = rmm.rmm_cupy_allocator(0)
+    assert m.mem.size == 0
+    assert m.mem.ptr == 0
+    assert m.mem.rmm_array is None
 
     cupy.cuda.set_allocator(rmm.rmm_cupy_allocator)
     a = cupy.arange(10)
