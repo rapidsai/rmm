193361a3e60a5b9b3e9adeddd0104a2878181c3b
Ashwin Srinath <shwina@users.noreply.github.com>
2019-12-20 09:18:53 -0500

    Make ptr and stream properties, add docstring to DevicePointer

--- python/rmm/_lib/device_pointer.pxd
+++ python/rmm/_lib/device_pointer.pxd
@@ -1,6 +1,5 @@
 from rmm._lib.lib cimport c_free, cudaStream_t
 
 cdef class DevicePointer:
-    cdef dict __dict__
     cdef void* c_ptr
     cdef cudaStream_t c_stream
--- python/rmm/_lib/device_pointer.pyx
+++ python/rmm/_lib/device_pointer.pyx
@@ -1,13 +1,32 @@
 from rmm._lib.lib cimport c_free, cudaStream_t
+from libc.stdint cimport uintptr_t
+
 
 cdef class DevicePointer:
     def __cinit__(self, ptr, stream=0):
-        self.c_ptr = <void*><size_t>(ptr)
-        self.c_stream = <cudaStream_t><size_t>(stream)
+        """
+        A DevicePointer wraps a raw pointer, freeing it
+        via `rmmFree()` when it (the DevicePointer) goes out of
+        scope. Effectively, DevicePointer takes ownership of the
+        memory pointed to by the pointer.
 
-    def __init__(self, ptr, stream=0):
-        self.ptr = ptr
-        self.stream = stream
+        Paramters
+        ---------
+        ptr : int
+            Pointer to device memory
+        stream : int, optional
+            CUDA stream to use for the deallocation
+            """
+        self.c_ptr = <void*><uintptr_t>(ptr)
+        self.c_stream = <cudaStream_t><uintptr_t>(stream)
+
+    @property
+    def ptr(self):
+        return int(<uintptr_t>(self.c_ptr))
+
+    @property
+    def stream(self):
+        return int(<uintptr_t>(self.c_stream))
 
     def __dealloc__(self):
         c_free(self.c_ptr, self.c_stream)
