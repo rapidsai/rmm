f43fe5c0e0323eae1068ff671753f6ad4a9b8f57
John Kirkham <jakirkham@gmail.com>
2020-01-24 18:41:42 -0800

    Test `DeviceBuffer.__sizeof__` method

--- python/rmm/tests/test_rmm.py
+++ python/rmm/tests/test_rmm.py
@@ -1,4 +1,5 @@
 from itertools import product
+import sys
 
 import numpy as np
 import pytest
@@ -94,6 +95,7 @@ def test_rmm_device_buffer(size):
     assert len(b) == b.size
     assert b.nbytes == b.size
     assert b.capacity() >= b.size
+    assert sys.getsizeof(b) == b.size
 
     # Test `__cuda_array_interface__`
     keyset = {"data", "shape", "strides", "typestr", "version"}
