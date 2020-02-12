917e1e26cce072308e81f8356a5e2baa1c88d29c
Jake Hemstad <jhemstad@nvidia.com>
2020-02-12 10:36:16 -0600

    Add aligned_allocate/deallocate.

--- include/rmm/detail/aligned.hpp
+++ include/rmm/detail/aligned.hpp
@@ -16,7 +16,10 @@
 
 #pragma once
 
+#include <cstddef>
 #include <cstdint>
+#include <memory>
+#include <new>
 
 namespace rmm {
 namespace detail {
@@ -34,12 +37,19 @@ constexpr bool is_supported_alignment(std::size_t alignment) {
  * alignment `alignment` using the unary callable `alloc` to allocate memory.
  *
  * Given a pointer `p` to an allocation of size `n` returned from the unary
- * callable `alloc`, the pointer `q` returned from `aligned_alloc` points to the
- * first location within the `n` bytes that satisfies `alignment`.
+ * callable `alloc`, the pointer `q` returned from `aligned_alloc` points to a
+ * location within the `n` bytes with sufficient space for `bytes` that
+ * satisfies `alignment`.
  *
- * In order to retrieve the original allocation pointer `p`, the difference
- * between `p` and `q` is stored in the `sizeof(std::size_t)` bytes at location
- * `q + bytes`.
+ * In order to retrieve the original allocation pointer `p`, the offset
+ * between `p` and `q` is stored at `q - sizeof(std::ptrdiff_t)`.
+ *
+ * Allocations returned from `aligned_allocate` *MUST* be free'd by calling
+ * `aligned_deallocate` with the same arguments for `bytes` and `alignment` with
+ * a compatible unary `dealloc` callable capable of freeing the memory returned
+ * from `alloc`.
+ *
+ * If `alignment` is not a power of 2, behavior is undefined.
  *
  * @param bytes The desired size of the allocation
  * @param alignment Desired alignment of allocation
@@ -50,51 +60,57 @@ constexpr bool is_supported_alignment(std::size_t alignment) {
  */
 template <typename A>
 void *aligned_allocate(std::size_t bytes, std::size_t alignment, A alloc) {
-  // don't allocate anything if the user requested zero bytes
-  if (0 == bytes) {
-    return nullptr;
-  }
+  assert(is_pow2(alignment));
 
   // allocate memory for bytes, plus potential alignment correction,
   // plus store of the correction offset
-  void *p = alloc(bytes + alignment + sizeof(std::size_t));
+  std::size_t padded_allocation_size{bytes + alignment +
+                                     sizeof(std::ptrdiff_t)};
 
-  std::size_t ptr_int = reinterpret_cast<std::size_t>(p);
+  char *const original = static_cast<char *>(alloc(padded_allocation_size));
 
-  // calculate the offset, i.e. how many bytes of correction were necessary
-  // to get an aligned pointer
-  std::size_t offset =
-      (ptr_int % alignment) ? (alignment - ptr_int % alignment) : 0;
-  // calculate the return pointer
-  char *ptr = static_cast<char *>(p) + offset;
-  // store the offset right after the actually returned value
-  std::size_t *offset_store = reinterpret_cast<std::size_t *>(ptr + bytes);
-  *offset_store = offset;
-  return static_cast<void *>(ptr);
+  // account for storage of offset immediate prior to the aligned pointer
+  void *aligned{original + sizeof(std::ptrdiff_t)};
+
+  // std::align modifies `aligned` to point to the first aligned location
+  if (not std::align(alignment, bytes, aligned, padded_allocation_size)) {
+    throw std::bad_alloc{};
+  }
+
+  // Compute the offset between the original and aligned pointers
+  std::ptrdiff_t offset = static_cast<char *>(aligned) - original;
+
+  // Store the offset immediately before the aligned pointer
+  *(static_cast<std::ptrdiff_t *>(aligned) - 1) = offset;
+
+  return aligned;
 }
 
 /**
- * @brief
+ * @brief Frees an allocation returned from `aligned_allocate`.
  *
- * @tparam D
- * @param p
- * @param bytes
- * @param alignment
- * @param dealloc
+ * Allocations returned from `aligned_allocate` *MUST* be free'd by calling
+ * `aligned_deallocate` with the same arguments for `bytes` and `alignment`
+ * with a compatible unary `dealloc` callable capable of freeing the memory
+ * returned from `alloc`.
+ *
+ * @param p The aligned pointer to deallocate
+ * @param bytes The number of bytes requested from `aligned_allocate`
+ * @param alignment The alignment required from `aligned_allocate`
+ * @param dealloc A unary callable capable of freeing memory returned from
+ * `alloc` in `aligned_allocate`.
  */
 template <typename D>
 void aligned_deallocate(void *p, std::size_t bytes, std::size_t alignment,
                         D dealloc) {
   (void)alignment;
-  if (nullptr == p) {
-    return;
-  }
-  char *ptr = static_cast<char *>(p);
-  // calculate where the offset is stored
-  std::size_t *offset = reinterpret_cast<std::size_t *>(ptr + bytes);
-  // calculate the original pointer
-  p = static_cast<void *>(ptr - *offset);
-  dealloc(p);
+
+  // Get offset from the location immediately prior to the aligned pointer
+  std::ptrdiff_t const offset = *(reinterpret_cast<std::ptrdiff_t *>(p) - 1);
+
+  void *const original = static_cast<char *>(p) - offset;
+
+  dealloc(original);
 }
 }  // namespace detail
 }  // namespace rmm
