c9f1dd1c28ed011c065cf5a3352d8967d3ba6351
Jake Hemstad <jhemstad@nvidia.com>
2020-02-13 11:19:11 -0600

    Example alloc_then_free benchmark.

--- benchmarks/allocations/allocations.cpp
+++ benchmarks/allocations/allocations.cpp
@@ -25,13 +25,54 @@
 #include <iostream>
 #include <thread>
 
-static void BM_test(benchmark::State& state) {
+struct allocation {
+  void* p{};
+  std::size_t size{};
+  allocation(void* _p, std::size_t _size) : p{_p}, size{_size} {}
+  allocation() = default;
+};
 
+/**
+ * @brief Performs a set of allocations of sizes (in bytes) determined by the
+ * range `[sizes_begin, sizes_end)` and then frees all of the allocations.
+ *
+ * Requires a pre-allocated `scratch` vector of at least size
+ * `std::distance(sizes_begin, sizes_end)` to track each allocation. This is to
+ * eliminate the overhead of allocating the scratch vector from this benchmark.
+ *
+ * @tparam SizeIterator Input iterator
+ * @param mr Resource to use for allocation/free
+ * @param sizes_begin Start of range of allocation sizes
+ * @param sizes_end End of range of allocation sizes
+ * @param scratch Preallocated vector of `allocation`s of at least size
+ * `std::distance(sizes_begin, sizes_end)` used for tracking each allocation.
+ */
+template <typename SizeIterator>
+void alloc_then_free(rmm::mr::device_memory_resource* mr,
+                     SizeIterator sizes_begin, SizeIterator sizes_end,
+                     std::vector<allocation>& scratch) {
+  // Do all allocations
+  std::transform(
+      sizes_begin, sizes_end, scratch.begin(),
+      [mr](std::size_t allocation_size) {
+        return allocation{mr->allocate(allocation_size), allocation_size};
+      });
+
+  // Free all allocations
+  std::for_each(scratch.begin(), scratch.end(),
+                [mr](allocation a) { mr->deallocate(a.p, a.size); });
+}
+
+static void BM_test(benchmark::State& state) {
   rmm::mr::cuda_memory_resource mr;
+
+  std::vector<std::size_t> sizes(100);
+  std::iota(sizes.begin(), sizes.end(), 1);
+  std::vector<allocation> allocation_scratch(sizes.size());
 
   try {
     for (auto _ : state) {
-      std::this_thread::sleep_for(std::chrono::duration<int>(1));
+      alloc_then_free(&mr, sizes.begin(), sizes.end(), allocation_scratch);
     }
   } catch (std::exception const& e) {
     std::cout << "Error: " << e.what() << "\n";
