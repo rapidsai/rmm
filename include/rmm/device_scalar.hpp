02c37335510d5c824c1ad8ccb1aa373cd733d3bf
Jake Hemstad <jhemstad@nvidia.com>
2020-02-14 13:44:58 -0600

    Update device_scalar to use CUDA_TRY.

--- include/rmm/device_scalar.hpp
+++ include/rmm/device_scalar.hpp
@@ -38,6 +38,10 @@ class device_scalar {
   /**---------------------------------------------------------------------------*
    * @brief Construct a new `device_scalar`
    *
+   * @throws `rmm::bad_alloc` if allocating the device memory for
+   *`initial_value` fails
+   * @throws `rmm::cuda_error` if copying `initial_value` to device memory fails
+   *
    * @param initial_value The initial value of the object in device memory
    * @param stream Optional, stream on which to perform allocation and copy
    * @param mr Optional, resource with which to allocate
@@ -46,13 +50,15 @@ class device_scalar {
       T const &initial_value, cudaStream_t stream = 0,
       rmm::mr::device_memory_resource *mr = rmm::mr::get_default_resource())
       : buff{sizeof(T), stream, mr} {
-    
     _memcpy(buff.data(), &initial_value, stream);
   }
 
   /**---------------------------------------------------------------------------*
    * @brief Copies the value from device to host, synchronizes, and returns the
    * value.
+   *
+   * @throws `rmm::cuda_error` If the copy fails
+   * @throws `rmm::cuda_error` If synchronizing `stream` fails
    *
    * @return T The value of the scalar after synchronizing the stream
    * @param stream CUDA stream on which to perform the copy
@@ -65,6 +71,9 @@ class device_scalar {
 
   /**---------------------------------------------------------------------------*
    * @brief Copies the value from host to device and synchronizes the stream.
+   *
+   * @throws `rmm::cuda_error` if copying `host_value` to device memory fails
+   * @throws `rmm::cuda_error` if synchronizing `stream` fails
    *
    * @param host_value The host value which will be copied to device
    * @param stream CUDA stream on which to perform the copy
@@ -93,17 +102,9 @@ class device_scalar {
  private:
   rmm::device_buffer buff{sizeof(T)};
 
-  inline void _memcpy(void *dst, const void *src,
-                      cudaStream_t stream) const{
-    auto status = cudaMemcpyAsync(dst, src, sizeof(T), cudaMemcpyDefault, stream);
-
-    if (cudaSuccess != status) {
-      throw std::runtime_error{"Device memcpy failed."};
-    }
-
-    if (cudaSuccess != cudaStreamSynchronize(stream)) {
-      throw std::runtime_error{"Stream sync failed."};
-    }
+  inline void _memcpy(void *dst, const void *src, cudaStream_t stream) const {
+    CUDA_TRY(cudaMemcpyAsync(dst, src, sizeof(T), cudaMemcpyDefault, stream));
+    CUDA_TRY(cudaStreamSynchronize(stream));
   }
 };
 
