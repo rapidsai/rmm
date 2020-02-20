9abd9856e3b4fba156967fb13461c00f83954115
Jake Hemstad <jhemstad@nvidia.com>
2020-02-20 12:06:21 -0600

    Add custom main to replay.

--- benchmarks/replay/replay.cpp
+++ benchmarks/replay/replay.cpp
@@ -19,8 +19,7 @@
 #include <benchmark/benchmark.h>
 
 static void BM_StringCreation(benchmark::State& state) {
-  for (auto _ : state)
-    std::string empty_string;
+  for (auto _ : state) std::string empty_string;
 }
 // Register the function as a benchmark
 BENCHMARK(BM_StringCreation);
@@ -28,34 +27,18 @@ BENCHMARK(BM_StringCreation);
 // Define another benchmark
 static void BM_StringCopy(benchmark::State& state) {
   std::string x = "hello";
-  for (auto _ : state)
-    std::string copy(x);
+  for (auto _ : state) std::string copy(x);
 }
 BENCHMARK(BM_StringCopy);
 
-static void BM_StringCompare(benchmark::State& state) {
-  std::string s1(state.range(0), '-');
-  std::string s2(state.range(0), '-');
-  for (auto _ : state) {
-    benchmark::DoNotOptimize(s1.compare(s2));
-  }
-  state.SetComplexityN(state.range(0));
-}
-BENCHMARK(BM_StringCompare)
-    ->RangeMultiplier(2)->Range(1<<10, 1<<18)->Complexity(benchmark::oN);
+int main(int argc, char** argv) {
 
-template <class Q> 
-void BM_Sequential(benchmark::State& state) {
-  Q q;
-  typename Q::value_type v(0);
-  for (auto _ : state) {
-    for (int i = state.range(0); i--; )
-      q.push_back(v);
-  }
-  // actually messages, not bytes:
-  state.SetBytesProcessed(
-      static_cast<int64_t>(state.iterations())*state.range(0));
-}
-BENCHMARK_TEMPLATE(BM_Sequential, std::vector<int>)->Range(1<<0, 1<<10); 
+  ::benchmark::Initialize(&argc, argv);
 
-BENCHMARK_MAIN();
+  // benchmark::Initialize will remove command line arguments it recognizes and
+  // leave any remaining arguments
+
+  // Parse for replay arguments:
+
+  ::benchmark::RunSpecifiedBenchmarks();
+}
