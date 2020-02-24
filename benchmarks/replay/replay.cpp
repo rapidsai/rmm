7cca8e878e3f24610a8073dc96ef2b0d32487ec6
Jake Hemstad <jhemstad@nvidia.com>
2020-02-24 15:45:40 -0600

    Get benchmark function ready.

--- benchmarks/replay/replay.cpp
+++ benchmarks/replay/replay.cpp
@@ -20,18 +20,10 @@
 #include <benchmark/benchmark.h>
 #include <string>
 
-static void BM_StringCreation(benchmark::State& state) {
-  for (auto _ : state) std::string empty_string;
+static void BM_Replay(benchmark::State& state) {
 }
-// Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
+BENCHMARK(BM_Replay);
 
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-  std::string x = "hello";
-  for (auto _ : state) std::string copy(x);
-}
-BENCHMARK(BM_StringCopy);
 
 enum class action : bool { ALLOCATE, FREE };
 
