ebb12ec9c8a6c730f31789e381650a8de7dc7f27
Mark Harris <mharris@nvidia.com>
2020-02-23 19:58:59 -0800

    hybrid_memory_resource now just has small and large upstream resources

--- include/rmm/mr/device/hybrid_memory_resource.hpp
+++ include/rmm/mr/device/hybrid_memory_resource.hpp
@@ -17,7 +17,6 @@
 
 #include <rmm/mr/device/device_memory_resource.hpp>
 #include <rmm/mr/device/default_memory_resource.hpp>
-#include <rmm/mr/device/fixed_size_memory_resource.hpp>
 #include <rmm/mr/device/sub_memory_resource.hpp>
 
 #include <cuda_runtime_api.h>
@@ -38,45 +37,23 @@ namespace rmm {
 
 namespace mr {
 
-namespace detail {
-  inline bool is_pow2(std::size_t x) { return (x & (x - 1)) == 0; }
-}
-
 /**---------------------------------------------------------------------------*
  * @brief Allocates fixed-size memory blocks.
  * 
  * Supports only allocations of size smaller than the configured block_size.
  *---------------------------------------------------------------------------**/
+ template <typename SmallAllocMemoryResource, typename LargeAllocMemoryResource>
 class hybrid_memory_resource : public device_memory_resource {
  public:
 
-  
-  static constexpr std::size_t default_min_fixed_size = 1 << 18; // 256 KiB
-  static constexpr std::size_t default_max_fixed_size = 1 << 22; // 4 MiB
+  static constexpr std::size_t default_max_small_size = 1 << 22; // 4 MiB
 
-  explicit hybrid_memory_resource(std::size_t min_fixed_size = default_min_fixed_size,
-                                  std::size_t max_fixed_size = default_max_fixed_size,
-                                  std::size_t initial_blocks_per_size = 128,
-                                  rmm::mr::device_memory_resource *heap_resource =
-                                    rmm::mr::get_default_resource()) 
-  : max_fixed_size_{max_fixed_size} {
-    if (!detail::is_pow2(min_fixed_size) || !detail::is_pow2(max_fixed_size)) 
-      throw std::logic_error("Only power of two fixed sizes supported");
-    
-    large_size_mr_ = new sub_memory_resource();
+  explicit hybrid_memory_resource(SmallAllocMemoryResource *small_mr,
+                                  LargeAllocMemoryResource *large_mr,
+                                  std::size_t max_small_size = default_max_small_size) 
+  : small_mr_{small_mr}, large_mr_{large_mr}, max_small_size_{max_small_size} {}
 
-    // allocate initial blocks and insert into free list
-    for (std::size_t i = min_fixed_size; i <= max_fixed_size; i *= 2) {
-      fixed_size_mr_.emplace_back(
-        new fixed_size_memory_resource(i, initial_blocks_per_size * i, large_size_mr_)
-      );
-    }
-  }
-
-  virtual ~hybrid_memory_resource() {
-    fixed_size_mr_.clear(); // must be deleted first since fixed_size_mrs use large_size_mr_
-    delete large_size_mr_;
-  }
+  virtual ~hybrid_memory_resource() = default;
 
   /**---------------------------------------------------------------------------*
    * @brief Queries whether the resource supports use of non-null streams for
@@ -86,19 +63,14 @@ class hybrid_memory_resource : public device_memory_resource {
    *---------------------------------------------------------------------------**/
   bool supports_streams() const noexcept override { return true; }
 
+  SmallAllocMemoryResource* get_small_mr() { return small_mr_; }
+  LargeAllocMemoryResource* get_large_mr() { return large_mr_; }
+
  private:
 
   rmm::mr::device_memory_resource& get_allocator(std::size_t bytes) {
-    if (bytes > max_fixed_size_) return *large_size_mr_;
-    else {
-      auto iter = fixed_size_mr_.begin();
-      while (bytes > iter->get()->get_block_size())
-        iter = std::next(iter);
-      if (iter != fixed_size_mr_.end())
-        return *iter->get();
-      else
-        throw std::bad_alloc();
-    }
+    if (bytes <= max_small_size_) return *small_mr_;
+    else return *large_mr_;
   }
 
   /**---------------------------------------------------------------------------*
@@ -143,13 +115,10 @@ class hybrid_memory_resource : public device_memory_resource {
     return std::make_pair(0, 0);
   }
 
-  std::size_t max_fixed_size_; // power of 2 maximum fixed size allocator
+  std::size_t max_small_size_; // power of 2 maximum fixed size allocator
 
-  sub_memory_resource *large_size_mr_; // allocator for larger than max_fixed_size
-
-  // allocators for fixed-size blocks <= max_fixed_size_
-  std::vector<std::unique_ptr<fixed_size_memory_resource>> fixed_size_mr_;
-  
+  SmallAllocMemoryResource *small_mr_; // allocator for <= max_small_size
+  LargeAllocMemoryResource *large_mr_; // allocator for > max_small_size
 };
 }  // namespace mr
 }  // namespace rmm
