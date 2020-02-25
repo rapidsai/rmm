a7bbe27ebc59803d82e09b1d2aab0ba679b8668f
Jake Hemstad <jhemstad@nvidia.com>
2020-02-24 19:11:43 -0600

    Replay working. Results are suspect as CUDA is too fast.

--- benchmarks/replay/replay.cpp
+++ benchmarks/replay/replay.cpp
@@ -17,6 +17,9 @@
 #include "cxxopts.hpp"
 #include "rapidcsv.h"
 
+#include <rmm/mr/device/cnmem_memory_resource.hpp>
+#include <rmm/mr/device/cuda_memory_resource.hpp>
+
 #include <benchmark/benchmark.h>
 #include <string>
 
@@ -34,7 +37,13 @@ enum class action : bool { ALLOCATE, FREE };
 struct parsed_log {
   parsed_log(std::vector<action>&& a, std::vector<std::size_t>&& s,
              std::vector<uintptr_t>&& p)
-      : actions{std::move(a)}, sizes{std::move(s)}, pointers{std::move(p)} {}
+      : actions{std::move(a)}, sizes{std::move(s)}, pointers{std::move(p)} {
+    if ((actions.size() != sizes.size()) or
+        (actions.size() != pointers.size())) {
+      throw std::runtime_error{
+          "Size mismatch between actions, sizes, pointers."};
+    }
+  }
   std::vector<action> actions{};
   std::vector<std::size_t> sizes{};
   std::vector<uintptr_t> pointers{};
@@ -75,10 +84,40 @@ parsed_log parse_csv(std::string const& filename) {
   return parsed_log{std::move(actions), std::move(sizes), std::move(pointers)};
 }
 
-void BM_takes_args(benchmark::State& state, std::string const& filename) {
-  for (auto _ : state) {
+struct allocation {
+  allocation() = default;
+  allocation(void* p_, std::size_t size_) : p{p_}, size{size_} {}
+  void* p{};
+  std::size_t size{};
+};
+
+template <typename MR>
+struct replay_benchmark {
+  MR mr_{};
+  parsed_log const& log_{};
+
+  replay_benchmark(MR const& mr, parsed_log const& log) : mr_{mr}, log_{log} {}
+
+  void operator()(benchmark::State& state) {
+    std::unordered_map<uintptr_t, allocation> allocation_map;
+
+    auto const& actions = log_.actions;
+    auto const& sizes = log_.sizes;
+    auto const& pointers = log_.pointers;
+
+    for (auto _ : state) {
+      for (int i = 0; i < actions.size(); ++i) {
+        if (action::ALLOCATE == actions[i]) {
+          auto p = mr_.allocate(sizes[i]);
+          allocation_map[pointers[i]] = allocation{p, sizes[i]};
+        } else {
+          auto a = allocation_map[pointers[i]];
+          mr_.deallocate(a.p, a.size);
+        }
+      }
+    }
   }
-}
+};
 
 int main(int argc, char** argv) {
   // benchmark::Initialize will remove GBench command line arguments it
@@ -100,11 +139,24 @@ int main(int argc, char** argv) {
     auto filename = result["file"].as<std::string>();
     auto parsed_log = parse_csv(filename);
 
-    auto func = BM_takes_args;
-    benchmark::RegisterBenchmark("test", func, filename);
+    std::cout << "Number of actions: " << parsed_log.actions.size()
+              << std::endl;
+
+    benchmark::RegisterBenchmark(
+        "CUDA Resource",
+        replay_benchmark<rmm::mr::cuda_memory_resource>{
+            rmm::mr::cuda_memory_resource{}, parsed_log})
+        ->Unit(benchmark::kMillisecond);
+
+    /*
+        benchmark::RegisterBenchmark(
+            "CNMEM Resource",
+            replay_benchmark<rmm::mr::cnmem_memory_resource>{
+                rmm::mr::cnmem_memory_resource{}, parsed_log})
+            ->Unit(benchmark::kMillisecond);
+            */
+    ::benchmark::RunSpecifiedBenchmarks();
   } else {
     throw std::runtime_error{"No log filename specified."};
   }
-
-  ::benchmark::RunSpecifiedBenchmarks();
 }
