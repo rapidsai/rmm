c2f871ab8397c295aeeb9004cac1a78031ef54a9
Jake Hemstad <jhemstad@nvidia.com>
2020-08-04 15:28:22 -0500

    Fix caps.

--- include/rmm/mr/device/per_device_resource.hpp
+++ include/rmm/mr/device/per_device_resource.hpp
@@ -80,7 +80,7 @@ inline cuda_device_id current_device()
  * Returns a pointer to the `device_memory_resource` for the specified device. The initial resource
  * is a `cuda_memory_resource`.
  *
- * `id.value()` must be in the range `[0, cudaGetDevicecount())`, otherwise behavior is undefined.
+ * `id.value()` must be in the range `[0, cudaGetDeviceCount())`, otherwise behavior is undefined.
  *
  * This function is thread-safe with respect to concurrent calls to `set_per_device_resource`,
  * `get_per_device_resource`, `get_current_device_resource`, and `set_current_device_resource`.
@@ -105,7 +105,7 @@ inline device_memory_resource* get_per_device_resource(cuda_device_id id)
  * If `new_mr` is not `nullptr`, sets the memory resource pointer for the device specified by `id`
  * to `new_mr`. Otherwise, resets `id`s resource to the initial `cuda_memory_resource`.
  *
- * `id.value()` must be in the range `[0, cudaGetDevicecount())`, otherwise behavior is undefined.
+ * `id.value()` must be in the range `[0, cudaGetDeviceCount())`, otherwise behavior is undefined.
  *
  * The object pointed to by `new_mr` must outlive the last use of the resource, otherwise behavior
  * is undefined. It is the caller's responsibility to maintain the lifetime of the resource object.
