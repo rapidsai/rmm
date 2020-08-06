eb29ca0d2ca122b54d3c7b88b7cb477cb96f2fd6
Raymond Douglass <ray@raydouglass.com>
2020-08-06 15:36:27 -0400

    DOC v0.16 Updates

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+# RMM 0.16.0 (Date TBD)
+
+## New Features
+
+## Improvements
+
+## Bug Fixes
+
 # RMM 0.15.0 (Date TBD)
 
 ## New Features
--- python/setup.py
+++ python/setup.py
@@ -86,7 +86,7 @@ extensions += cythonize(
 
 setup(
     name="rmm",
-    version="0.15.0",
+    version="0.16.0",
     description="rmm - RAPIDS Memory Manager",
     url="https://github.com/rapidsai/rmm",
     author="NVIDIA Corporation",
