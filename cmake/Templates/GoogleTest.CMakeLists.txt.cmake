0f1e8e89ea89817258654d3bebcd4e57985b97d8
Keith Kraus <kkraus@nvidia.com>
2020-09-10 22:19:55 -0400

    fix tests cmake for cxx11 abi, update gtest version to 1.10.0

--- cmake/Templates/GoogleTest.CMakeLists.txt.cmake
+++ cmake/Templates/GoogleTest.CMakeLists.txt.cmake
@@ -5,9 +5,9 @@ include(ExternalProject)
 
 ExternalProject_Add(GoogleTest
                     GIT_REPOSITORY    https://github.com/google/googletest.git
-                    GIT_TAG           release-1.8.0
+                    GIT_TAG           release-1.10.0
                     SOURCE_DIR        "${GTEST_ROOT}/googletest"
                     BINARY_DIR        "${GTEST_ROOT}/build"
-                    INSTALL_DIR		  "${GTEST_ROOT}/install"
+                    INSTALL_DIR	      "${GTEST_ROOT}/install"
                     CMAKE_ARGS        ${GTEST_CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=${GTEST_ROOT}/install)
 
