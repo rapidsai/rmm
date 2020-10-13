51dd4053e1350e1463c111c528b428d6fd5bd766
Keith Kraus <kkraus@nvidia.com>
2020-10-13 16:56:32 -0400

    match clang-format settings of 100 width, 2 spaces tabs

--- cmake/EvalGPUArchs.cmake
+++ cmake/EvalGPUArchs.cmake
@@ -1,28 +1,25 @@
 # Copyright (c) 2019, NVIDIA CORPORATION.
 #
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not
-# use this file except in compliance with the License. You may obtain a copy of
-# the License at
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
 #
 # http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations under
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied. See the License for the specific language governing permissions and limitations under
 # the License.
 #
 
-# Function uses the CUDA runtime API to query the compute capability of the
-# device, so if a user doesn't pass any architecture options to CMake we only
-# build the current architecture
+# Function uses the CUDA runtime API to query the compute capability of the device, so if a user
+# doesn't pass any architecture options to CMake we only build the current architecture
 function(evaluate_gpu_archs gpu_archs)
-    set(eval_file ${PROJECT_BINARY_DIR}/eval_gpu_archs.cu)
-    set(eval_exe ${PROJECT_BINARY_DIR}/eval_gpu_archs)
-    set(error_file ${PROJECT_BINARY_DIR}/eval_gpu_archs.stderr.log)
-    file(
-        WRITE ${eval_file}
-        "
+  set(eval_file ${PROJECT_BINARY_DIR}/eval_gpu_archs.cu)
+  set(eval_exe ${PROJECT_BINARY_DIR}/eval_gpu_archs)
+  set(error_file ${PROJECT_BINARY_DIR}/eval_gpu_archs.stderr.log)
+  file(
+    WRITE ${eval_file}
+    "
 #include <cstdio>
 #include <set>
 #include <string>
@@ -52,14 +49,13 @@ int main(int argc, char** argv) {
   return 0;
 }
 ")
-    execute_process(
-        COMMAND ${CMAKE_CUDA_COMPILER} -std=c++11 -o ${eval_exe} --run
-                ${eval_file}
-        OUTPUT_VARIABLE __gpu_archs
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-        ERROR_FILE ${error_file})
-    message("Auto detection of gpu-archs: ${__gpu_archs}")
-    set(${gpu_archs}
-        ${__gpu_archs}
-        PARENT_SCOPE)
+  execute_process(
+    COMMAND ${CMAKE_CUDA_COMPILER} -std=c++11 -o ${eval_exe} --run ${eval_file}
+    OUTPUT_VARIABLE __gpu_archs
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_FILE ${error_file})
+  message("Auto detection of gpu-archs: ${__gpu_archs}")
+  set(${gpu_archs}
+      ${__gpu_archs}
+      PARENT_SCOPE)
 endfunction(evaluate_gpu_archs)
