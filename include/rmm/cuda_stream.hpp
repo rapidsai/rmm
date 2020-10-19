89939213a8545bb07ec625f3ef4d6bafc119fa60
Mark Harris <mharris@nvidia.com>
2020-10-19 15:32:55 +1100

    Add cuda_stream owning wrapper

---
+++ include/rmm/cuda_stream.hpp
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2020, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <rmm/cuda_stream_view.hpp>
+#include <rmm/detail/error.hpp>
+
+#include <cuda_runtime_api.h>
+
+namespace rmm {
+
+/**
+ * @brief Owning wrapper for a CUDA stream.
+ *
+ * Provides RAII lifetime semantics for a CUDA stream.
+ *
+ */
+class cuda_stream {
+ public:
+  // No copy construction or assignment
+  cuda_stream(cuda_stream const&) = delete;
+  cuda_stream& operator=(cuda_stream const&) = delete;
+
+  // Move construction and assignment allowed
+  cuda_stream(cuda_stream&&) = default;
+  cuda_stream& operator=(cuda_stream&&) = default;
+
+  /**
+   * @brief Construct a new cuda stream object
+   *
+   * @throw rmm::cuda_error if stream creation fails
+   */
+  cuda_stream() : stream_{} { RMM_CUDA_TRY(cudaStreamCreate(&stream_)); }
+
+  /**
+   * @brief Destroy the cuda_stream object and the CUDA stream it owns.
+   *
+   */
+  ~cuda_stream() noexcept { RMM_ASSERT_CUDA_SUCCESS(cudaStreamDestroy(stream_)); }
+
+  /**
+   * @brief Creates an immutable, non-owning view of the CUDA stream.
+   *
+   * @return rmm::cuda_stream_view The view of the CUDA stream
+   */
+  constexpr rmm::cuda_stream_view view() const { return rmm::cuda_stream_view{stream_}; }
+
+  /**
+   * @brief Implicit conversion to cudaStream_t.
+   */
+  constexpr operator cudaStream_t() const noexcept { return stream_; }
+
+  /**
+   * @brief Implicit conversion to cuda_stream_view
+   *
+   * @return A view of the owned stream
+   */
+  constexpr operator cuda_stream_view() const { return view(); }
+
+  /**
+   * @brief Synchronize the owned CUDA stream.
+   *
+   * Call's `cudaStreamSynchronize()`.
+   *
+   * @throw rmm::cuda_error if stream synchronization fails
+   */
+  void synchronize() { RMM_CUDA_TRY(cudaStreamSynchronize(stream_)); }
+
+  /**
+   * @brief Compare two streams for equality.
+   */
+  constexpr bool operator==(cuda_stream const& other) const noexcept
+  {
+    return stream_ == other.stream_;
+  }
+
+  /**
+   * @brief Compare two streams for equality.
+   */
+  constexpr bool operator==(cuda_stream_view const& other) const noexcept
+  {
+    return stream_ == other;
+  }
+
+ private:
+  cudaStream_t stream_;
+};
+
+}  // namespace rmm
