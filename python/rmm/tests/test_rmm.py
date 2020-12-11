9884459f8674a7e37540ec4c4b3821dbfc909454
Peter Andreas Entschev <peter@entschev.com>
2020-12-11 14:24:01 -0800

    Fix test for DeviceBuffer.__sizeof__

--- python/rmm/tests/test_rmm.py
+++ python/rmm/tests/test_rmm.py
@@ -109,7 +109,9 @@ def test_rmm_device_buffer(size):
     assert len(b) == b.size
     assert b.nbytes == b.size
     assert b.capacity() >= b.size
-    assert sys.getsizeof(b) == b.size
+
+    # b.size + DeviceBuffer attributes overhead
+    assert sys.getsizeof(b) > b.size
 
     # Test `__cuda_array_interface__`
     keyset = {"data", "shape", "strides", "typestr", "version"}
