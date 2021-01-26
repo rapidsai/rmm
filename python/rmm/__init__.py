e899c68b35b21cda63b8eac6209746dd8cccde86
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2021-01-26 16:38:04 +0900

    Expose the version string in C++ and Python (#666)
    Closes #663
    
    Depends on #665
    
    * Expose C++ macros `RMM_VER_MAJOR` and `RMM_VER_MINOR` to encode the major and minor versions of RMM. This is useful for downstream packages that links with RMM in the C++ layer.
    * Expose the version string via `rmm.__version__`, to follow a widely-recognized convention for accessing version of Python modules.
    
    Authors:
      - Philip Hyunsu Cho (@hcho3)
    
    Approvers:
      - Ashwin Srinath (@shwina)
      - @jakirkham
      - Keith Kraus (@kkraus14)
      - Mark Harris (@harrism)
    
    URL: https://github.com/rapidsai/rmm/pull/666

--- python/rmm/__init__.py
+++ python/rmm/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018-2019, NVIDIA CORPORATION.
+# Copyright (c) 2018-2021, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -24,3 +24,7 @@ from rmm.rmm import (
     reinitialize,
     rmm_cupy_allocator,
 )
+
+from rmm._version import get_versions
+
+__version__ = get_versions()["version"]
