f59cb92e66a0121531035e1eb9ff6c8925ade16d
Ashwin Srinath <shwina@users.noreply.github.com>
2021-03-02 18:51:19 -0500

    Fix error  message

--- include/rmm/mr/device/cuda_async_memory_resource.hpp
+++ include/rmm/mr/device/cuda_async_memory_resource.hpp
@@ -47,11 +47,11 @@ class cuda_async_memory_resource final : public device_memory_resource {
     RMM_CUDA_TRY(cudaGetDevice(&device));
     int v{0};
     auto e = cudaDeviceGetAttribute(&v, cudaDevAttrMemoryPoolsSupported, device);
-    RMM_EXPECTS(
-      e == cudaSuccess && v == 1,
-      "cudaMallocAsync not supported by the version of the CUDA Toolkit used for this build");
+    RMM_EXPECTS(e == cudaSuccess && v == 1,
+                "cudaMallocAsync not supported with this CUDA driver/runtime version");
 #else
-    RMM_FAIL("cudaMallocAsync not supported by the version of the CUDA Toolkit used for this build");
+    RMM_FAIL(
+      "cudaMallocAsync not supported by the version of the CUDA Toolkit used for this build");
 #endif
   }
 
