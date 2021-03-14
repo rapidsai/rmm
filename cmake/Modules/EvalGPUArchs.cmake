cb81c801d0d64c55ef0487e5640e4ddfe85a2f3a
Paul Taylor <paul.e.taylor@me.com>
2021-03-13 19:56:55 -0600

    Fix auto-detecting GPU architectures (#727)
    Fixes regression from https://github.com/rapidsai/rmm/pull/726 in auto-detecting GPU architectures when `-DCMAKE_CUDA_ARCHITECTURES=` is passed on the CLI.
    
    Now that the cached `CMAKE_CUDA_ARCHITECTURES` isn't unset before calling `enable_language(CUDA)`, this call throws an error and configuration fails. This change ensures we call `enable_language(CUDA)` after any potential rewrites of `CMAKE_CUDA_ARCHITECTURES`.
    
    Authors:
      - Paul Taylor (@trxcllnt)
    
    Approvers:
      - Keith Kraus (@kkraus14)
    
    URL: https://github.com/rapidsai/rmm/pull/727

--- cmake/Modules/EvalGPUArchs.cmake
+++ cmake/Modules/EvalGPUArchs.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2019-2020, NVIDIA CORPORATION.
+# Copyright (c) 2019-2021, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
 # in compliance with the License. You may obtain a copy of the License at
@@ -9,6 +9,12 @@
 # is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 # or implied. See the License for the specific language governing permissions and limitations under
 # the License.
+
+# Unset this first in case it's set to <empty_string>
+unset(CMAKE_CUDA_ARCHITECTURES CACHE)
+
+# Enable CUDA so we can invoke nvcc
+enable_language(CUDA)
 
 # Function uses the CUDA runtime API to query the compute capability of the device, so if a user
 # doesn't pass any architecture options to CMake we only build the current architecture
