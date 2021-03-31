f8e9050d7e12b75c816c494bf0ea4d94482f7fdb
galipremsagar <sagarprem75@gmail.com>
2021-03-31 09:22:12 -0700

    correct the python pinnings

--- python/setup.py
+++ python/setup.py
@@ -201,8 +201,8 @@ setup(
         "Topic :: Scientific/Engineering",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     # Include the separately-compiled shared library
     setup_requires=["Cython>=0.29,<0.30"],
