6a4b42f67dd23bcdf8858d556ab537503490b51b
Rong Ou <rong.ou@gmail.com>
2021-04-19 14:29:55 -0700

    update to 1.8.5

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -42,8 +42,8 @@ conda activate rapids
 gpuci_conda_retry install rapids-build-env=${MINOR_VERSION}.*
 
 # https://docs.rapids.ai/maintainers/depmgmt/ 
-conda remove --force rapids-build-env
-gpuci_conda_retry install "spdlog=1.8.2" -c conda-forge
+# conda remove -f rapids-build-env
+# gpuci_conda_retry install "your-pkg=1.0.0"
 
 gpuci_logger "Check versions"
 python --version
