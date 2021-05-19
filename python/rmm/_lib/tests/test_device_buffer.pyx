df69121f175cda2f0b5e6a35dffc541d7ed61dda
Mark Harris <mharris@nvidia.com>
2021-05-19 17:49:46 +1000

    Fix Cython

--- python/rmm/_lib/tests/test_device_buffer.pyx
+++ python/rmm/_lib/tests/test_device_buffer.pyx
@@ -18,14 +18,17 @@ import numpy as np
 from libcpp.memory cimport make_unique
 from libcpp.utility cimport move
 
+from rmm._lib.cuda_stream_view cimport cuda_stream_default
+
 from rmm._lib.device_buffer cimport DeviceBuffer, device_buffer
 
 
 def test_release():
     expect = DeviceBuffer.to_device(b'abc')
     cdef DeviceBuffer buf = DeviceBuffer.to_device(b'abc')
+
     got = DeviceBuffer.c_from_unique_ptr(
-        make_unique[device_buffer](buf.c_release())
+        make_unique[device_buffer](buf.c_release(), cuda_stream_default.value())
     )
     np.testing.assert_equal(expect.copy_to_host(), got.copy_to_host())
 
