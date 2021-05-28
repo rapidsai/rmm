a225ce57dc050f05b75a6efa373eccd5e0bdbd00
Robert Maynard <rmaynard@nvidia.com>
2021-05-28 12:25:56 -0400

    RMM now leverages rapids-cmake to reduce CMake boilerplate
    rapids-cmake provides all the functionality of RMM existing
    CMake modules. This allow RAPIDS to deploy build-system fixes quickly
    and across all projects.

---
+++ cmake/thirdparty/get_thrust.cmake
@@ -0,0 +1,33 @@
+#=============================================================================
+# Copyright (c) 2021, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#=============================================================================
+
+function(find_and_configure_thrust VERSION)
+
+    rapids_cpm_find(Thrust ${VERSION}
+        BUILD_EXPORT_SET    rmm-exports
+        INSTALL_EXPORT_SET  rmm-exports
+        CPM_ARGS
+            GIT_REPOSITORY https://github.com/NVIDIA/thrust.git
+            GIT_TAG        ${VERSION}
+            GIT_SHALLOW TRUE
+            OPTIONS "THRUST_INSTALL TRUE"
+    )
+
+    thrust_create_target(rmm::Thrust FROM_OPTIONS)
+
+endfunction()
+
+find_and_configure_thrust(1.12.0)
