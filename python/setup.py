47f55227ce9483b78e51eee1a05382ba08165583
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2021-06-01 12:23:46 -0700

    Compile Cython with C++17. (#787)
    #741 moved the code base over to C++17, and as discussed in #736 moving Cython compilation over is fine as well but has not been done yet. There's some use of structured bindings in there now (in `cuda_async_memory_resource.hpp`) so the Cython compilation should be switched over as well.
    
    Authors:
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    Approvers:
      - GALI PREM SAGAR (https://github.com/galipremsagar)
      - https://github.com/jakirkham
      - Mark Harris (https://github.com/harrism)
    
    URL: https://github.com/rapidsai/rmm/pull/787

--- python/setup.py
+++ python/setup.py
@@ -132,7 +132,7 @@ extensions = cythonize(
             ],
             libraries=["cuda", "cudart"],
             language="c++",
-            extra_compile_args=["-std=c++14"],
+            extra_compile_args=["-std=c++17"],
         )
     ],
     nthreads=nthreads,
