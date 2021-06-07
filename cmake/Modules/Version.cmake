e2832e3034a21f7eef40b9c15e17c2761d4d249c
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2021-06-07 14:09:34 -0700

    Remove leading zeros in version_config.hpp (#793)
    Since we switched to CalVer, the version strings contain leading zeros, e.g. `21.08.00`. This leads to the generated header `version_config.hpp` to contain
    ```cpp
    #define RMM_VERSION_MAJOR 21
    #define RMM_VERSION_MINOR 08
    #define RMM_VERSION_PATCH 00
    ```
    in which `08` is an invalid octal literal. See the resulting CI failure at https://gpuci.gpuopenanalytics.com/job/rapidsai/job/conda/job/xgboost/job/xgboost-conda-build/229/CUDA=11.0,PYTHON=3.7/console. The generated header is used by XGBoost.
    
    Fix: Remove the leading zeros in the C++ constants so that we'd get
    ```cpp
    ```cpp
    #define RMM_VERSION_MAJOR 21
    #define RMM_VERSION_MINOR 8
    #define RMM_VERSION_PATCH 0
    ```
    
    Authors:
      - Philip Hyunsu Cho (https://github.com/hcho3)
    
    Approvers:
      - Jake Hemstad (https://github.com/jrhemstad)
    
    URL: https://github.com/rapidsai/rmm/pull/793

--- cmake/Modules/Version.cmake
+++ cmake/Modules/Version.cmake
@@ -13,6 +13,9 @@
 # Generate version_config.hpp from the version found in CMakeLists.txt
 function(write_version)
   message(STATUS "RMM VERSION: ${RMM_VERSION}")
+  math(EXPR RMM_VERSION_MAJOR_CPP_HEADER "${RMM_VERSION_MAJOR} + 0" OUTPUT_FORMAT DECIMAL)
+  math(EXPR RMM_VERSION_MINOR_CPP_HEADER "${RMM_VERSION_MINOR} + 0" OUTPUT_FORMAT DECIMAL)
+  math(EXPR RMM_VERSION_PATCH_CPP_HEADER "${RMM_VERSION_PATCH} + 0" OUTPUT_FORMAT DECIMAL)
   configure_file(${RMM_SOURCE_DIR}/cmake/version_config.hpp.in
                  ${RMM_BINARY_DIR}/include/rmm/version_config.hpp @ONLY)
 endfunction(write_version)
