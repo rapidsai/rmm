f7dccee9d6e1eaf2208353459e9dea31243defc7
Mark Harris <mharris@nvidia.com>
2021-10-13 14:12:21 +1100

    Add simple resource_view test

---
+++ tests/resource_view_tests.cpp
@@ -0,0 +1,20 @@
+#include <rmm/mr/device/cuda_memory_resource.hpp>
+#include <rmm/mr/device/device_memory_resource.hpp>
+
+#include <gtest/gtest.h>
+
+struct ResourceViewTest : public ::testing::Test {
+};
+
+void foo(rmm::mr::device_memory_resource* mr) {}
+
+void bar(cuda::resource_view<cuda::memory_access::device> mr) {}
+
+TEST_F(ResourceViewTest, InterchangeableWithResourcePointer)
+{
+  rmm::mr::cuda_memory_resource mr;
+
+  foo(&mr);
+  bar(&mr);
+  bar(cuda::view_resource(&mr));
+}
