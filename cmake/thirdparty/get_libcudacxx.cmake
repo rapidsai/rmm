0a6e9c6d80c2f375742e9711ffe175e48dc0ea9e
Mark Harris <mharris@nvidia.com>
2021-10-13 15:41:39 +1100

    cmake formatting

--- cmake/thirdparty/get_libcudacxx.cmake
+++ cmake/thirdparty/get_libcudacxx.cmake
@@ -17,7 +17,7 @@ function(find_and_configure_libcudacxx VERSION)
   rapids_cpm_find(
     libcudacxx ${VERSION}
     GIT_REPOSITORY https://github.com/mzient/libcudacxx.git
-    GIT_TAG memres_view  # ${VERSION}
+    GIT_TAG memres_view # ${VERSION}
     GIT_SHALLOW TRUE DOWNLOAD_ONLY TRUE)
 
   set(LIBCUDACXX_INCLUDE_DIR
