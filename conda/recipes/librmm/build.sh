e9aefba59019b16c983a45c87f5fe19c90bd6ca0
AJ Schmidt <aschmidt@nvidia.com>
2021-11-08 11:43:46 -0500

    add {has,no}_cma variants back to `librmm`

--- conda/recipes/librmm/build.sh
+++ conda/recipes/librmm/build.sh
@@ -1,4 +1,9 @@
 # Copyright (c) 2018-2019, NVIDIA CORPORATION.
 
 # This assumes the script is executed from the root of the repo directory
-./build.sh -v clean librmm --cmake-args=\"-DCMAKE_INSTALL_LIBDIR=lib\"
+BUILD_FLAGS=""
+if [ "${cudaMallocAsync}" == "no_cma" ]; then
+    BUILD_FLAGS="${BUILD_FLAGS} --no-cudamallocasync"
+fi
+
+./build.sh -v clean librmm --cmake-args=\"-DCMAKE_INSTALL_LIBDIR=lib\" ${BUILD_FLAGS}
