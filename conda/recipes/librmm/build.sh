b1a29d85e67e7a7ca698b73490d20086e09ae823
Jordan Jacobelli <jjacobelli@nvidia.com>
2022-01-17 11:19:39 +0100

    Enable tests build for Project Flash
    Signed-off-by: Jordan Jacobelli <jjacobelli@nvidia.com>

--- conda/recipes/librmm/build.sh
+++ conda/recipes/librmm/build.sh
@@ -1,4 +1,8 @@
 # Copyright (c) 2018-2019, NVIDIA CORPORATION.
 
 # This assumes the script is executed from the root of the repo directory
-./build.sh -v clean librmm --cmake-args=\"-DCMAKE_INSTALL_LIBDIR=lib\"
+if [[ -z "$PROJECT_FLASH" || "$PROJECT_FLASH" == "0" ]]; then
+  ./build.sh -v clean librmm --cmake-args=\"-DCMAKE_INSTALL_LIBDIR=lib\"
+else
+  ./build.sh -v clean librmm tests --cmake-args=\"-DCMAKE_INSTALL_LIBDIR=lib\"
+fi
