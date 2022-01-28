26df82527d5a3d8cb755543353bb01690a83e97b
AJ Schmidt <aschmidt@nvidia.com>
2022-01-27 19:25:36 -0500

    use consistent build paths

--- ci/cpu/build.sh
+++ ci/cpu/build.sh
@@ -66,18 +66,18 @@ conda config --set ssl_verify False
 if [[ "$BUILD_LIBRMM" == "1" ]]; then
   gpuci_logger "Build conda pkg for librmm"
   if [[ -z "$PROJECT_FLASH" || "$PROJECT_FLASH" == "0" ]]; then
-    gpuci_conda_retry build conda/recipes/librmm --python=$PYTHON
+    gpuci_conda_retry build --no-build-id conda/recipes/librmm --python=$PYTHON
   else
-    gpuci_conda_retry build --dirty --no-remove-work-dir conda/recipes/librmm
+    gpuci_conda_retry build --no-build-id --dirty --no-remove-work-dir conda/recipes/librmm
   fi
 fi
 
 if [[ "$BUILD_RMM" == "1" ]]; then
   gpuci_logger "Build conda pkg for rmm"
   if [[ -z "$PROJECT_FLASH" || "$PROJECT_FLASH" == "0" ]]; then
-    gpuci_conda_retry build conda/recipes/rmm --python=$PYTHON
+    gpuci_conda_retry build --no-build-id conda/recipes/rmm --python=$PYTHON
   else
-    gpuci_conda_retry build --dirty --no-remove-work-dir \
+    gpuci_conda_retry build --no-build-id --dirty --no-remove-work-dir \
       -c $WORKSPACE/ci/artifacts/rmm/cpu/conda-bld/ conda/recipes/rmm
 
   fi
