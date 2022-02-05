41d600f9b8868ab01efd6b48349eea14ae970b4f
Vyas Ramasubramani <vyasr@nvidia.com>
2022-02-04 18:29:17 -0800

    Apply linters.

--- python/cmake/rmm-python-helpers.cmake
+++ python/cmake/rmm-python-helpers.cmake
@@ -1,14 +1,31 @@
+#[=======================================================================[.rst:
+add_cython_modules
+------------------
+
+Generate C(++) from Cython and create Python modules.
+
+.. code-block:: cmake
+
+  add_cython_modules(<ModuleName...>)
+
+Creates a Cython target for a module, then adds a corresponding Python
+extension module.
+
+``ModuleName``
+  The list of modules to build.
+
+#]=======================================================================]
 function(add_cython_modules cython_modules)
   foreach(cython_module ${cython_modules})
     add_cython_target(${cython_module} CXX PY3)
     add_library(${cython_module} MODULE ${cython_module})
-    # TODO: This doesn't seem to be necessary for some reason. Are the Python
-    # headers somehow being included without it?
+    # TODO: This doesn't seem to be necessary for some reason. Are the Python headers somehow being
+    # included without it?
     #python_extension_module(${cython_module})
-    
+
     # To avoid libraries being prefixed with "lib".
     set_target_properties(${cython_module} PROPERTIES PREFIX "")
-    
+
     target_link_libraries(${cython_module} rmm::rmm Python3::Module)
     target_include_directories(${cython_module} PRIVATE "${CMAKE_CURRENT_SOURCE_DIR}")
     install(TARGETS ${cython_module} DESTINATION rmm/_lib)
