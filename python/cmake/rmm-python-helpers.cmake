ee69827de5dbaff2570589a6d16848e327efa8d2
Vyas Ramasubramani <vyasr@nvidia.com>
2022-02-04 18:06:02 -0800

    Move cython module adding logic to a function.

---
+++ python/cmake/rmm-python-helpers.cmake
@@ -0,0 +1,16 @@
+function(add_cython_modules cython_modules)
+  foreach(cython_module ${cython_modules})
+    add_cython_target(${cython_module} CXX PY3)
+    add_library(${cython_module} MODULE ${cython_module})
+    # TODO: This doesn't seem to be necessary for some reason. Are the Python
+    # headers somehow being included without it?
+    #python_extension_module(${cython_module})
+    
+    # To avoid libraries being prefixed with "lib".
+    set_target_properties(${cython_module} PROPERTIES PREFIX "")
+    
+    target_link_libraries(${cython_module} rmm::rmm Python3::Module)
+    target_include_directories(${cython_module} PRIVATE "${CMAKE_CURRENT_SOURCE_DIR}")
+    install(TARGETS ${cython_module} DESTINATION rmm/_lib)
+  endforeach(cython_module ${cython_sources})
+endfunction()
