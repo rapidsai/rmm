e5b1e43ad9bd88f7f3f310500da39812e5663519
Vyas Ramasubramani <vyasr@nvidia.com>
2022-02-10 13:24:47 -0800

    Some basic cleanup.

--- python/cmake/rmm-python-helpers.cmake
+++ python/cmake/rmm-python-helpers.cmake
@@ -30,20 +30,14 @@ extension module.
 
 #]=======================================================================]
 function(add_cython_modules cython_modules install_dst)
-  message("The install destination is ${install_dst} when the cython module is ${cython_modules}")
   foreach(cython_module ${cython_modules})
     add_cython_target(${cython_module} CXX PY3)
     add_library(${cython_module} MODULE ${cython_module})
-    # TODO: This fails unless the C language is enabled in the project.Also,
-    # this is slower to compile than just linking manually to Python3 below.
     python_extension_module(${cython_module})
 
     # To avoid libraries being prefixed with "lib".
     set_target_properties(${cython_module} PROPERTIES PREFIX "")
 
-    # TODO: This is the "manual" version of the python_extension_module command
-    # above, but it seems to compile faster.
-    #target_link_libraries(${cython_module} rmm::rmm Python3::Module)
     target_link_libraries(${cython_module} rmm::rmm)
     target_include_directories(${cython_module} PRIVATE "${CMAKE_CURRENT_SOURCE_DIR}")
     # TODO: Currently requiring the user to pass the install destination
