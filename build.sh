5436109c0ada74e2ee48004a5c135c980fde6df7
Vyas Ramasubramani <vyasr@nvidia.com>
2022-04-14 15:12:16 -0700

    Change BUILD_DIR to BUILD_DIRS so that clean works correctly.

--- build.sh
+++ build.sh
@@ -37,7 +37,7 @@ HELP="$0 [clean] [librmm] [rmm] [-v] [-g] [-n] [-s] [--ptds] [--cmake-args=\"<ar
 "
 LIBRMM_BUILD_DIR=${LIBRMM_BUILD_DIR:=${REPODIR}/build}
 RMM_BUILD_DIR="${REPODIR}/python/build ${REPODIR}/python/_skbuild"
-BUILD_DIR="${LIBRMM_BUILD_DIR} ${RMM_BUILD_DIR}"
+BUILD_DIRS="${LIBRMM_BUILD_DIR} ${RMM_BUILD_DIR}"
 
 # Set defaults for vars modified by flags to this script
 VERBOSE_FLAG=""
