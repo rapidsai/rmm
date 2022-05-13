e60311ad7fcfea9da73da16ed9950632861a9517
ayodeawe <jawe@nvidia.com>
2022-05-13 02:32:18 -0500

    cleanup

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -1,26 +1,8 @@
 #!/bin/bash
 set -e
 
-################################################################################
-# SETUP - Check environment
-################################################################################
-
-gpuci_logger "Get env"
-env
-
-gpuci_logger "Activate conda env"
-. /opt/conda/etc/profile.d/conda.sh
-conda activate rapids
-
-gpuci_logger "Check versions"
-python --version
-$CC --version
-$CXX --version
-
-gpuci_logger "Check conda environment"
-conda info
-conda config --show-sources
-conda list --show-channel-urls
+# Check environment
+source ci/check_environment.sh
 
 ################################################################################
 # BUILD - Conda package builds (LIBRMM)
@@ -32,5 +14,4 @@ conda build \
   --output-folder /tmp/conda-bld-output \
   conda/recipes/librmm
 
-# doc -> https://github.com/rapidsai/gpuci-tools/pull/26#issue-1226701276
 rapids-upload-conda-to-s3 cpp
--- ci/gpu_test.sh
+++ ci/gpu_test.sh
@@ -1,28 +1,8 @@
 #!/bin/bash
 set -e
 
-################################################################################
-# SETUP - Check environment
-################################################################################
-
-gpuci_logger "Get env"
-env
-
-gpuci_logger "Activate conda env"
-. /opt/conda/etc/profile.d/conda.sh
-conda activate rapids
-
-echo "CUDA version for mamba install: ${CUDA}"
-
-gpuci_logger "Check versions"
-python --version
-$CC --version
-$CXX --version
-
-gpuci_logger "Check conda environment"
-conda info
-conda config --show-sources
-conda list --show-channel-urls
+# Check environment
+source ci/check_environment.sh
 
 
 # GPU Test Stage
