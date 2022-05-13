c0494b6a03619d4e2262d4f24a3b44d8a7e44afb
AJ Schmidt <aschmidt@nvidia.com>
2022-05-13 09:44:16 -0400

    Revert "test building in non-rapids environment"
    This reverts commit 23e992fb2feaf95d675f1e8c5a38e7e5ef8f6ab0.

--- ci/check_environment.sh
+++ ci/check_environment.sh
@@ -3,6 +3,10 @@
 gpuci_logger "Get env"
 env
 
+gpuci_logger "Activate conda env"
+. /opt/conda/etc/profile.d/conda.sh
+conda activate rapids
+
 gpuci_logger "Check versions"
 python --version
 $CC --version
