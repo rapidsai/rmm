eb9e5a08adf20d99b2c270b8cc53b85b4e8f3bee
John Kirkham <jakirkham@gmail.com>
2022-05-18 16:35:24 -0700

    Require `numpy` version `1.19+`
    This is consistent with RAPIDS own NumPy version constraint.

--- python/setup.py
+++ python/setup.py
@@ -34,6 +34,6 @@ setup(
         )
     },
     cmdclass=versioneer.get_cmdclass(),
-    install_requires=["cuda-python>=11.5,<12.0", "numpy", "numba"],
+    install_requires=["cuda-python>=11.5,<12.0", "numpy>=1.19", "numba"],
     zip_safe=False,
 )
