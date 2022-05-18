e3c4151e3e81d4d2c5437550d81011593ad223c0
John Kirkham <jakirkham@gmail.com>
2022-05-18 16:34:44 -0700

    Sort `install_requires`

--- python/setup.py
+++ python/setup.py
@@ -34,6 +34,6 @@ setup(
         )
     },
     cmdclass=versioneer.get_cmdclass(),
-    install_requires=["numba", "cuda-python"],
+    install_requires=["cuda-python", "numba"],
     zip_safe=False,
 )
