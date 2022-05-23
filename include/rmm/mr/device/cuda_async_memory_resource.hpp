c9e10a6dc18e6843d30ed062fb506ea5f086be81
Mark Harris <mharris@nvidia.com>
2022-05-23 14:56:34 -0700

    Document allocation_handle_type

--- include/rmm/mr/device/cuda_async_memory_resource.hpp
+++ include/rmm/mr/device/cuda_async_memory_resource.hpp
@@ -45,7 +45,14 @@ namespace rmm::mr {
 class cuda_async_memory_resource final : public device_memory_resource {
  public:
   /**
-   * Flags for specifying particular handle types
+   * @brief Flags for specifying memory allocation handle types.
+   *
+   * @note These values are exact copies from `cudaMemAllocationHandleType`. We need to
+   * define our own enum here because the earliest CUDA runtime version that supports asynchronous
+   * memory pools (CUDA 11.2) did not support these flags. So we need a placeholder that can be
+   * used consistently in the constructor of `cuda_async_memory_resource` with all versions of
+   * CUDA >= 11.2. See the `cudaMemAllocationHandleType` docs at
+   * https://docs.nvidia.com/cuda/cuda-runtime-api/group__CUDART__TYPES.html"
    */
   enum class allocation_handle_type {
     none                  = 0x0,  ///< Does not allow any export mechanism.
