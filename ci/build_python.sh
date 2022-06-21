ce32132f3dc496370f0c8acfc57e43e8a6e13364
Jordan Jacobelli <jjacobelli@nvidia.com>
2022-06-21 10:09:48 +0200

    Add GitHub Actions workflows
    Signed-off-by: Jordan Jacobelli <jjacobelli@nvidia.com>

---
+++ ci/build_python.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+source rapids-env-update
+
+rapids-print-env
+
+rapids-logger "Begin py build"
+
+CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
+
+rapids-mamba-retry mambabuild -c "${CPP_CHANNEL}" conda/recipes/rmm
+
+rapids-upload-conda-to-s3 python
