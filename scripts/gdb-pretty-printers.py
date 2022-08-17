69eb5d27620c82c2a91a05efce8cd9b50cbfe2af
Tobias Ribizel <tribizel@nvidia.com>
2022-08-17 18:20:30 +0000

    provide script to load pretty-printers

--- scripts/gdb-pretty-printers.py
+++ scripts/gdb-pretty-printers.py
@@ -16,137 +16,138 @@
 import gdb
 
 
-if not 'ThrustVectorPrinter' in dir():
-    print("This file expects the Thrust pretty-printers to be loaded already. "
-          "Either load them manually, or use the generated gdb-pretty-printers.py "
-          "in the build directory")
-else:
-    class HostIterator(Iterator):
-        """Iterates over arrays in host memory"""
-
-        def __init__(self, start, size):
-            self.item = start
-            self.size = size
-            self.count = 0
-
-        def __iter__(self):
-            return self
-
-        def __next__(self):
-            if self.count >= self.size:
-                raise StopIteration
-            elt = self.item.dereference()
-            count = self.count
-            self.item = self.item + 1
-            self.count = self.count + 1
-            return ('[%d]' % count, elt)
+if not 'ThrustVectorPrinter' in locals():
+    raise Exception("This file expects the Thrust pretty-printers to be loaded already. "
+                    "Either load them manually, or use the generated gdb-pretty-printers.py "
+                    "in the build directory")
 
 
-    class DeviceIterator(Iterator):
-        """Iterates over arrays in device memory by copying chunks into host memory"""
+class HostIterator(Iterator):
+    """Iterates over arrays in host memory"""
 
-        def __init__(self, start, size):
-            self.exec = exec
-            self.item = start
-            self.size = size
-            self.count = 0
-            self.buffer = None
-            self.sizeof = self.item.dereference().type.sizeof
-            self.buffer_start = 0
-            # At most 1 MB or size, at least 1
-            self.buffer_size = min(size, max(1, 2 ** 20 // self.sizeof))
-            self.buffer = gdb.parse_and_eval(
-                '(void*)malloc(%s)' % (self.buffer_size * self.sizeof))
-            self.buffer.fetch_lazy()
-            self.buffer_count = self.buffer_size
-            self.update_buffer()
+    def __init__(self, start, size):
+        self.item = start
+        self.size = size
+        self.count = 0
 
-        def update_buffer(self):
-            if self.buffer_count >= self.buffer_size:
-                self.buffer_item = gdb.parse_and_eval(
-                    hex(self.buffer)).cast(self.item.type)
-                self.buffer_count = 0
-                self.buffer_start = self.count
-                device_addr = hex(self.item.dereference().address)
-                buffer_addr = hex(self.buffer)
-                size = min(self.buffer_size, self.size -
-                            self.buffer_start) * self.sizeof
-                status = gdb.parse_and_eval(
-                    '(cudaError)cudaMemcpy(%s, %s, %d, cudaMemcpyDeviceToHost)' % (buffer_addr, device_addr, size))
-                if status != 0:
-                    raise gdb.MemoryError(
-                        'memcpy from device failed: %s' % status)
+    def __iter__(self):
+        return self
 
-        def __del__(self):
-            gdb.parse_and_eval('(void)free(%s)' %
-                                hex(self.buffer)).fetch_lazy()
-
-        def __iter__(self):
-            return self
-
-        def __next__(self):
-            if self.count >= self.size:
-                raise StopIteration
-            self.update_buffer()
-            elt = self.buffer_item.dereference()
-            self.buffer_item = self.buffer_item + 1
-            self.buffer_count = self.buffer_count + 1
-            count = self.count
-            self.item = self.item + 1
-            self.count = self.count + 1
-            return ('[%d]' % count, elt)
+    def __next__(self):
+        if self.count >= self.size:
+            raise StopIteration
+        elt = self.item.dereference()
+        count = self.count
+        self.item = self.item + 1
+        self.count = self.count + 1
+        return ('[%d]' % count, elt)
 
 
-    class RmmDeviceUVectorPrinter(gdb.printing.PrettyPrinter):
-        """Print a rmm::device_uvector"""
+class DeviceIterator(Iterator):
+    """Iterates over arrays in device memory by copying chunks into host memory"""
 
-        def __init__(self, val):
-            self.val = val
-            el_type = val.type.template_argument(0)
-            self.pointer = val['_storage']['_data'].cast(el_type.pointer())
-            self.size = int(val['_storage']['_size']) // el_type.sizeof
-            self.capacity = int(val['_storage']['_capacity']) // el_type.sizeof
+    def __init__(self, start, size):
+        self.exec = exec
+        self.item = start
+        self.size = size
+        self.count = 0
+        self.buffer = None
+        self.sizeof = self.item.dereference().type.sizeof
+        self.buffer_start = 0
+        # At most 1 MB or size, at least 1
+        self.buffer_size = min(size, max(1, 2 ** 20 // self.sizeof))
+        self.buffer = gdb.parse_and_eval(
+            '(void*)malloc(%s)' % (self.buffer_size * self.sizeof))
+        self.buffer.fetch_lazy()
+        self.buffer_count = self.buffer_size
+        self.update_buffer()
 
-        def children(self):
-            return DeviceIterator(self.pointer, self.size)        
+    def update_buffer(self):
+        if self.buffer_count >= self.buffer_size:
+            self.buffer_item = gdb.parse_and_eval(
+                hex(self.buffer)).cast(self.item.type)
+            self.buffer_count = 0
+            self.buffer_start = self.count
+            device_addr = hex(self.item.dereference().address)
+            buffer_addr = hex(self.buffer)
+            size = min(self.buffer_size, self.size -
+                        self.buffer_start) * self.sizeof
+            status = gdb.parse_and_eval(
+                '(cudaError)cudaMemcpy(%s, %s, %d, cudaMemcpyDeviceToHost)' % (buffer_addr, device_addr, size))
+            if status != 0:
+                raise gdb.MemoryError(
+                    'memcpy from device failed: %s' % status)
 
-        def to_string(self):
-            typename = str(self.val.type)
-            return ('%s of length %d, capacity %d' % (typename, self.size, self.capacity))
+    def __del__(self):
+        gdb.parse_and_eval('(void)free(%s)' %
+                            hex(self.buffer)).fetch_lazy()
 
-        def display_hint(self):
-            return 'array'
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.count >= self.size:
+            raise StopIteration
+        self.update_buffer()
+        elt = self.buffer_item.dereference()
+        self.buffer_item = self.buffer_item + 1
+        self.buffer_count = self.buffer_count + 1
+        count = self.count
+        self.item = self.item + 1
+        self.count = self.count + 1
+        return ('[%d]' % count, elt)
 
 
-    # Workaround to avoid using the pretty printer on things like std::vector<int>::iterator
-    def is_template_type_not_alias(typename):
-        loc = typename.find('<')
-        if loc is None:
+class RmmDeviceUVectorPrinter(gdb.printing.PrettyPrinter):
+    """Print a rmm::device_uvector"""
+
+    def __init__(self, val):
+        self.val = val
+        el_type = val.type.template_argument(0)
+        self.pointer = val['_storage']['_data'].cast(el_type.pointer())
+        self.size = int(val['_storage']['_size']) // el_type.sizeof
+        self.capacity = int(val['_storage']['_capacity']) // el_type.sizeof
+
+    def children(self):
+        return DeviceIterator(self.pointer, self.size)        
+
+    def to_string(self):
+        typename = str(self.val.type)
+        return ('%s of length %d, capacity %d' % (typename, self.size, self.capacity))
+
+    def display_hint(self):
+        return 'array'
+
+
+# Workaround to avoid using the pretty printer on things like std::vector<int>::iterator
+def is_template_type_not_alias(typename):
+    loc = typename.find('<')
+    if loc is None:
+        return False
+    depth = 0
+    for char in typename[typename.find('<'):-1]:
+        if char == '<':
+            depth += 1
+        if char == '>':
+            depth -= 1
+        if depth == 0:
             return False
-        depth = 0
-        for char in typename[typename.find('<'):-1]:
-            if char == '<':
-                depth += 1
-            if char == '>':
-                depth -= 1
-            if depth == 0:
-                return False
-        return True
+    return True
 
 
-    def template_match(typename, template_name):
-        return typename.startswith(template_name + "<") and typename.endswith(">")
+def template_match(typename, template_name):
+    return typename.startswith(template_name + "<") and typename.endswith(">")
 
 
-    def lookup_rmm_type(val):
-        if not str(val.type.unqualified()).startswith('rmm::'):
-            return None
-        suffix = str(val.type.unqualified())[5:]
-        if not is_template_type_not_alias(suffix):
-            return None
-        if template_match(suffix, 'device_uvector'):
-            return RmmDeviceUVectorPrinter(val)
+def lookup_rmm_type(val):
+    if not str(val.type.unqualified()).startswith('rmm::'):
         return None
+    suffix = str(val.type.unqualified())[5:]
+    if not is_template_type_not_alias(suffix):
+        return None
+    if template_match(suffix, 'device_uvector'):
+        return RmmDeviceUVectorPrinter(val)
+    return None
 
 
-    gdb.pretty_printers.append(lookup_rmm_type)
+gdb.pretty_printers.append(lookup_rmm_type)
