14743e6efa88175584d13cd667f55739af70f3e8
jakirkham <jakirkham@gmail.com>
2022-11-07 19:33:13 -0800

    Keep license metadata & add license file in setup

--- python/setup.py
+++ python/setup.py
@@ -4,6 +4,7 @@ from setuptools import find_packages
 from skbuild import setup
 
 setup(
+    license_files=["LICENSE"],
     packages=find_packages(include=["rmm", "rmm.*"]),
     zip_safe=False,
 )
