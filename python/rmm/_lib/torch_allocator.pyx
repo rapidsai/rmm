2b343c2dbea27170d0dd565acc5e5a610c83117b
Ashwin Srinath <shwina@users.noreply.github.com>
2023-01-05 09:17:46 -0500

    Correctly type stream parameter for allocate/deallocate

--- python/rmm/_lib/torch_allocator.pyx
+++ python/rmm/_lib/torch_allocator.pyx
@@ -1,5 +1,6 @@
 from cuda.ccudart cimport cudaStream_t
 
+from rmm._lib.cuda_stream_view cimport cuda_stream_view
 from rmm._lib.memory_resource cimport device_memory_resource
 from rmm._lib.per_device_resource cimport get_current_device_resource
 
@@ -8,10 +9,16 @@ cdef public void* allocate(
     ssize_t size, int device, void* stream
 ) except * with gil:
     cdef device_memory_resource* mr = get_current_device_resource()
-    return mr[0].allocate(size, <cudaStream_t> stream)
+    cdef cuda_stream_view stream_view = cuda_stream_view(
+        <cudaStream_t>(stream)
+    )
+    return mr[0].allocate(size, stream_view)
 
 cdef public void deallocate(
     void* ptr, ssize_t size, void* stream
 ) except * with gil:
     cdef device_memory_resource* mr = get_current_device_resource()
-    mr[0].deallocate(ptr, size, <cudaStream_t> stream)
+    cdef cuda_stream_view stream_view = cuda_stream_view(
+        <cudaStream_t>(stream)
+    )
+    mr[0].deallocate(ptr, size, stream_view)
