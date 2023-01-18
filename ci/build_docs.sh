598ac534de5f928f08b04e49721bbb497191b5fd
Jake Awe <jawe@nvidia.com>
2023-01-18 04:58:47 -0600

    update build_docs.sh

--- ci/build_docs.sh
+++ ci/build_docs.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-VERSION_NUMBER=$(echo `git describe --abbrev=0 --tags` | grep -o -E '([0-9]+\.[0-9]+)')
-
 set -euo pipefail
 
 rapids-logger "Create test conda environment"
@@ -20,6 +17,7 @@ rapids-print-env
 rapids-logger "Downloading artifacts from previous jobs"
 CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
 PYTHON_CHANNEL=$(rapids-download-conda-from-s3 python)
+VERSION_NUMBER=$(rapids-get-rapids-version-from-git)
 
 rapids-mamba-retry install \
   --channel "${CPP_CHANNEL}" \
@@ -28,17 +26,19 @@ rapids-mamba-retry install \
 
 # Build CPP docs
 rapids-logger "Build Doxygen docs"
-cd ./doxygen
+pushd ./doxygen
 doxygen Doxyfile
+popd
 
 # Build Python docs
 rapids-logger "Build Python docs"
-cd ../python/docs
+pushd python/docs
 sphinx-build -b dirhtml . _html
 sphinx-build -b text . _text
+popd
 
 if [[ ${RAPIDS_BUILD_TYPE} == "branch" ]]; then
-  aws s3 sync --delete _html "s3://rapidsai-docs/rmm/${VERSION_NUMBER}/html"
-  aws s3 sync --delete _text "s3://rapidsai-docs/rmm/${VERSION_NUMBER}/txt"
-  aws s3 sync --delete ../../doxygen/html "s3://rapidsai-docs/librmm/${VERSION_NUMBER}/html"
+  aws s3 sync --delete python/docs/_html "s3://rapidsai-docs/rmm/${VERSION_NUMBER}/html"
+  aws s3 sync --delete python/docs/_text "s3://rapidsai-docs/rmm/${VERSION_NUMBER}/txt"
+  aws s3 sync --delete doxygen/html "s3://rapidsai-docs/librmm/${VERSION_NUMBER}/html"
 fi
