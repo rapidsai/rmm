a786ed6a4a7603c41931295d84486e6bc22cbcf2
Vyas Ramasubramani <vyasr@nvidia.com>
2023-02-17 14:23:34 -0800

    Update wheel modification script.

--- ci/release/apply_wheel_modifications.sh
+++ ci/release/apply_wheel_modifications.sh
@@ -7,6 +7,6 @@ VERSION=${1}
 CUDA_SUFFIX=${2}
 
 sed -i "s/__version__ = .*/__version__ = \"${VERSION}\"/g" python/rmm/__init__.py
-sed -i "s/version=.*,/version=\"${VERSION}\",/g" python/setup.py
+sed -i "s/^version = .*/version = \"${VERSION}\"/g" python/pyproject.toml
 
-sed -i "s/name=\"rmm\",/name=\"rmm${CUDA_SUFFIX}\",/g" python/setup.py
+sed -i "s/^name = \"rmm\"/name = \"rmm${CUDA_SUFFIX}\"/g" python/pyproject.toml
