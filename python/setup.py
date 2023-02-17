691e3f8573ae7c15539806f3efb51d53894c84b8
Vyas Ramasubramani <vyasr@nvidia.com>
2023-02-17 13:54:35 -0800

    Revert "Move setuptools options back to `setup.py`"
    This reverts commit 6b2ca6ab79b1e7f450c323be5d47318f7752a118.

--- python/setup.py
+++ python/setup.py
@@ -1,9 +1,5 @@
 # Copyright (c) 2019-2022, NVIDIA CORPORATION.
 
-from setuptools import find_packages
 from skbuild import setup
 
-setup(
-    packages=find_packages(include=["rmm", "rmm.*"]),
-    zip_safe=False,
-)
+setup()
