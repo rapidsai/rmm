868968dd62219a14cf44e07c69413ffdae64d39c
Vyas Ramasubramani <vyasr@nvidia.com>
2023-02-17 13:54:30 -0800

    Revert "Keep license metadata & add license file in setup"
    This reverts commit 14743e6efa88175584d13cd667f55739af70f3e8.

--- python/setup.py
+++ python/setup.py
@@ -4,7 +4,6 @@ from setuptools import find_packages
 from skbuild import setup
 
 setup(
-    license_files=["LICENSE"],
     packages=find_packages(include=["rmm", "rmm.*"]),
     zip_safe=False,
 )
