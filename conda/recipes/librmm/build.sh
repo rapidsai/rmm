83207fb98301e42f25a863da68fbdf71fb08909c
Bradley Dice <bdice@bradleydice.com>
2023-02-27 14:03:02 -0600

    Add CUDAHOSTCXX before invoking CMake.

--- conda/recipes/librmm/build.sh
+++ conda/recipes/librmm/build.sh
@@ -1,3 +1,4 @@
 # Copyright (c) 2018-2022, NVIDIA CORPORATION.
 
+export CUDAHOSTCXX=${CXX}  # Needed for CUDA 12 nvidia channel compilers
 ./build.sh -n -v clean librmm tests benchmarks --cmake-args=\"-DCMAKE_INSTALL_LIBDIR=lib\"
