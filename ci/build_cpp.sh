8ba29de816b12728780a998bf36f787327c4423b
AJ Schmidt <aschmidt@nvidia.com>
2023-02-28 18:41:19 -0500

    test script changes

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -8,4 +8,10 @@ env | sort
 RAPIDS_ARTIFACTS_DIR=${RAPIDS_ARTIFACTS_DIR:-"${PWD}/artifacts"}
 mkdir -p "${RAPIDS_ARTIFACTS_DIR}"
 
+# Test latest script changes
+wget \
+  https://raw.githubusercontent.com/ajschmidt8/gha-tools/upload-artifacts-dir/tools/rapids-upload-artifacts-dir \
+  -O /usr/local/bin/rapids-upload-artifacts-dir
+
 echo "file contents" > "${RAPIDS_ARTIFACTS_DIR}"/somefile.txt
+echo "more file contents" > "${RAPIDS_ARTIFACTS_DIR}"/another_file.txt
