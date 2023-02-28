c1b6f9981d90014d888e5c6e6a270f2b16732808
Bradley Dice <bdice@bradleydice.com>
2023-02-28 15:24:44 -0600

    Use NVCC_PREPEND_FLAGS instead of CUDAHOSTCXX.

--- conda/recipes/librmm/build.sh
+++ conda/recipes/librmm/build.sh
@@ -1,4 +1,4 @@
 # Copyright (c) 2018-2022, NVIDIA CORPORATION.
 
-export CUDAHOSTCXX=${CXX}  # Needed for CUDA 12 nvidia channel compilers
+export NVCC_PREPEND_FLAGS="${NVCC_PREPEND_FLAGS} -ccbin ${CXX}" # Needed for CUDA 12 nvidia channel compilers
 ./build.sh -n -v clean librmm tests benchmarks --cmake-args=\"-DCMAKE_INSTALL_LIBDIR=lib\"
