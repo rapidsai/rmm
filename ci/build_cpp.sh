19fffd127c530e55888ecba9c23e762969edfb38
AJ Schmidt <aschmidt@nvidia.com>
2023-02-28 17:59:22 -0500

    edit `build_cpp.sh` to create artifact

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -3,14 +3,6 @@
 
 set -euo pipefail
 
-source rapids-env-update
-
-export CMAKE_GENERATOR=Ninja
-
 rapids-print-env
 
-rapids-logger "Begin cpp build"
-
-rapids-mamba-retry mambabuild conda/recipes/librmm
-
-rapids-upload-conda-to-s3 cpp
+echo "file contents" > "${RAPIDS_ARTIFACTS_DIR}"/somefile.txt
