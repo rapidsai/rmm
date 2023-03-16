ae01bb6d8e4d45ddd389eaff44abee2c0e321121
Vyas Ramasubramani <vyasr@nvidia.com>
2023-03-16 12:20:34 -0700

    Remove MANIFEST and rely on auto-generated one from scikit-build.

--- python/setup.py
+++ python/setup.py
@@ -1,10 +1,9 @@
-# Copyright (c) 2019-2022, NVIDIA CORPORATION.
+# Copyright (c) 2019-2023, NVIDIA CORPORATION.
 
 from setuptools import find_packages
 from skbuild import setup
 
 setup(
-    packages=find_packages(include=["rmm", "rmm.*"]),
-    include_package_data=True,
+    packages=find_packages(include=["rmm*"]),
     zip_safe=False,
 )
