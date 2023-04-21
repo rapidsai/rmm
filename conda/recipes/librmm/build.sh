e67075dffa77ecc1a86f5352bacdad9e48379943
Vyas Ramasubramani <vyasr@nvidia.com>
2023-04-21 10:56:00 -0700

    Try removing prepend flags

--- conda/recipes/librmm/build.sh
+++ conda/recipes/librmm/build.sh
@@ -1,4 +1,3 @@
 # Copyright (c) 2018-2022, NVIDIA CORPORATION.
 
-export NVCC_PREPEND_FLAGS="${NVCC_PREPEND_FLAGS} -ccbin ${CXX}" # Needed for CUDA 12 nvidia channel compilers
 ./build.sh -n -v clean librmm tests benchmarks --cmake-args=\"-DCMAKE_INSTALL_LIBDIR=lib\"
