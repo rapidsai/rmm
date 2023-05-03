cb541fe8e3ab39006c6c4715efd2696ba4e37bff
divyegala <divyegala@gmail.com>
2023-05-03 16:25:54 -0700

    cuda-python lower bound 12.0 instead of 12.1

--- ci/release/apply_wheel_modifications.sh
+++ ci/release/apply_wheel_modifications.sh
@@ -11,5 +11,5 @@ sed -i "s/^version = .*/version = \"${VERSION}\"/g" python/pyproject.toml
 sed -i "s/^name = \"rmm\"/name = \"rmm${CUDA_SUFFIX}\"/g" python/pyproject.toml
 
 if [[ $CUDA_SUFFIX == "-cu12" ]]; then
-    sed -i "/cuda-python/ s/>=11.7.1,<12.0/>=12.1,<13.0/g" python/pyproject.toml
+    sed -i "/cuda-python/ s/>=11.7.1,<12.0/>=12.0,<13.0/g" python/pyproject.toml
 fi
