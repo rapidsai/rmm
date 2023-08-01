29db492bcf65134e994bb3fe08a9ad290d0ba662
Vyas Ramasubramani <vyasr@nvidia.com>
2023-08-01 15:07:47 -0700

    Add numba to intersphinx

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -181,6 +181,7 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
+    "numba": ("https://numba.readthedocs.io/en/stable", None),
 }
 
 # Config numpydoc
