6d52424537b9fb16e31efbfdc2e4f2a4ae863cb2
Vyas Ramasubramani <vyasr@nvidia.com>
2023-08-01 14:56:50 -0700

    Fix intersphinx_mapping

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -179,7 +179,9 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {"https://docs.python.org/": None}
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+}
 
 # Config numpydoc
 numpydoc_show_inherited_class_members = True
