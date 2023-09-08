9ceab835b23a1b30f91a0adf4dcf709cc7312a44
Michael Schellenberger Costa <miscco@nvidia.com>
2023-09-08 14:14:48 +0200

    Make `pinned_memory_resource` usable for `pool_memory_resource`

--- include/rmm/mr/host/pinned_memory_resource.hpp
+++ include/rmm/mr/host/pinned_memory_resource.hpp
@@ -15,6 +15,7 @@
  */
 #pragma once
 
+#include <rmm/cuda_stream_view.hpp>
 #include <rmm/detail/aligned.hpp>
 #include <rmm/detail/error.hpp>
 #include <rmm/mr/host/host_memory_resource.hpp>
@@ -45,6 +46,78 @@ class pinned_memory_resource final : public host_memory_resource {
     default;  ///< @default_copy_assignment{pinned_memory_resource}
   pinned_memory_resource& operator=(pinned_memory_resource&&) =
     default;  ///< @default_move_assignment{pinned_memory_resource}
+
+  /**
+   * @brief Query whether the pinned_memory_resource supports use of non-null CUDA streams for
+   * allocation/deallocation.
+   *
+   * @returns bool false.
+   */
+  [[nodiscard]] bool supports_streams() const noexcept { return false; }
+
+  /**
+   * @brief Query whether the resource supports the get_mem_info API.
+   *
+   * @return bool false.
+   */
+  [[nodiscard]] bool supports_get_mem_info() const noexcept { return false; }
+
+  /**
+   * @brief Queries the amount of free and total memory for the resource.
+   *
+   * @param stream the stream whose memory manager we want to retrieve
+   *
+   * @returns a pair containing the free memory in bytes in .first and total amount of memory in
+   * .second
+   */
+  [[nodiscard]] std::pair<std::size_t, std::size_t> get_mem_info(cuda_stream_view stream) const
+  {
+    return std::make_pair(0, 0);
+  }
+
+  /**
+   * @brief Pretent to support the allocate_async interface, falling back to stream 0
+   *
+   * @throws `rmm::bad_alloc` When the requested `bytes` cannot be allocated on
+   * the specified `stream`.
+   *
+   * @param bytes The size of the allocation
+   * @param alignment The expected alignment of the allocation
+   * @return void* Pointer to the newly allocated memory
+   */
+  [[nodiscard]] void* allocate_async(std::size_t bytes, std::size_t alignment, cuda_stream_view)
+  {
+    return do_allocate(bytes, alignment);
+  }
+
+  /**
+   * @brief Pretent to support the allocate_async interface, falling back to stream 0
+   *
+   * @throws `rmm::bad_alloc` When the requested `bytes` cannot be allocated on
+   * the specified `stream`.
+   *
+   * @param bytes The size of the allocation
+   * @return void* Pointer to the newly allocated memory
+   */
+  [[nodiscard]] void* allocate_async(std::size_t bytes, cuda_stream_view)
+  {
+    return do_allocate(bytes);
+  }
+
+  /**
+   * @brief Pretent to support the deallocate_async interface, falling back to stream 0
+   *
+   * @throws Nothing.
+   *
+   * @param ptr Pointer to be deallocated
+   * @param bytes The size in bytes of the allocation. This must be equal to the
+   * value of `bytes` that was passed to the `allocate` call that returned `p`.
+   * @param alignment The alignment that was passed to the `allocate` call that returned `p`
+   */
+  void deallocate_async(void* ptr, std::size_t bytes, std::size_t alignment, cuda_stream_view)
+  {
+    do_deallocate(ptr, rmm::detail::align_up(bytes, alignment));
+  }
 
  private:
   /**
@@ -101,5 +174,6 @@ class pinned_memory_resource final : public host_memory_resource {
       ptr, bytes, alignment, [](void* ptr) { RMM_ASSERT_CUDA_SUCCESS(cudaFreeHost(ptr)); });
   }
 };
+static_assert(cuda::mr::async_resource<pinned_memory_resource>);
 /** @} */  // end of group
 }  // namespace rmm::mr
