fa0d563f9e12513cbd9a816aafa06c2abb84023d
Vyas Ramasubramani <vyasr@nvidia.com>
2023-09-22 11:12:27 -0700

    Also update conda builds

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -9,11 +9,17 @@ export CMAKE_GENERATOR=Ninja
 
 rapids-print-env
 
+# Use gha-tools rapids-pip-wheel-version to generate wheel version then
+# update the necessary files
+version_override=$(./ci/get_version.sh ${package_name} ${package_dir})
+#sed -i "s/^version = .*/version = ${version_override}/g" ${pyproject_file}
+echo "__version__ = ${version_override}" > ${package_dir}/${package_name}/_version.py
+
 rapids-logger "Begin py build"
 
 CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
 
 # This calls mambabuild when boa is installed (as is the case in the CI images)
-rapids-conda-retry mambabuild -c "${CPP_CHANNEL}" conda/recipes/rmm
+RAPIDS_PACKAGE_VERSION=version_override rapids-conda-retry mambabuild -c "${CPP_CHANNEL}" conda/recipes/rmm
 
 rapids-upload-conda-to-s3 python
