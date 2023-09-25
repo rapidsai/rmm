51bbbd62cdb64d20b5a28aeeb5f75f76c3c23814
Vyas Ramasubramani <vyasr@nvidia.com>
2023-09-25 09:48:11 -0700

    Revert code block changes

--- include/rmm/device_uvector.hpp
+++ include/rmm/device_uvector.hpp
@@ -39,7 +39,7 @@ namespace rmm {
  * `thrust::uninitialized_fill`.
  *
  * Example:
- * @code{.cpp}
+ * @code{c++}
  * rmm::mr::device_memory_resource * mr = new my_custom_resource();
  * rmm::cuda_stream_view s{};
  *
