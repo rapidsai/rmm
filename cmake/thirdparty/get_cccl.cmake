c75120b28cafbb9486716ff8594f25ac662e378f
Bradley Dice <bdice@bradleydice.com>
2023-12-10 22:16:59 -0600

    Fix name.

--- cmake/thirdparty/get_cccl.cmake
+++ cmake/thirdparty/get_cccl.cmake
@@ -12,7 +12,7 @@
 # the License.
 # =============================================================================
 
-# Use CPM to find or clone thrust
+# Use CPM to find or clone CCCL
 function(find_and_configure_cccl)
 
   include(${rapids-cmake-dir}/cpm/cccl.cmake)
