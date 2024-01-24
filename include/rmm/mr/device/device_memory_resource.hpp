27def4f217ae96dc7affabdba46e8cdc7c043eec
Mark Harris <783069+harrism@users.noreply.github.com>
2024-01-24 01:08:28 +0000

    Deprecate supports_get_mem_info and get_mem_info.

--- include/rmm/mr/device/device_memory_resource.hpp
+++ include/rmm/mr/device/device_memory_resource.hpp
@@ -306,7 +306,12 @@ class device_memory_resource {
    *
    * @return bool true if the resource supports get_mem_info, false otherwise.
    */
-  [[nodiscard]] virtual bool supports_get_mem_info() const noexcept { return false; };
+  [[deprecated("Use rmm::available_device_memory instead.")]]  //
+  [[nodiscard]] virtual bool
+  supports_get_mem_info() const noexcept
+  {
+    return false;
+  };
 
   /**
    * @brief Queries the amount of free and total memory for the resource.
@@ -316,9 +321,11 @@ class device_memory_resource {
    * @returns a pair containing the free memory in bytes in .first and total amount of memory in
    * .second
    */
-  [[nodiscard]] std::pair<std::size_t, std::size_t> get_mem_info(cuda_stream_view stream) const
+  [[deprecated("Use rmm::available_device_memory instead.")]]  //
+  [[nodiscard]] std::pair<std::size_t, std::size_t>
+  get_mem_info(cuda_stream_view stream) const
   {
-    return do_get_mem_info(stream);
+    return {0, 0};
   }
 
   /**
@@ -373,20 +380,6 @@ class device_memory_resource {
   [[nodiscard]] virtual bool do_is_equal(device_memory_resource const& other) const noexcept
   {
     return this == &other;
-  }
-
-  /**
-   * @brief Get free and available memory for memory resource
-   *
-   * @throws std::runtime_error if we could not get free / total memory
-   *
-   * @param stream the stream being executed on
-   * @return std::pair with available and free memory for resource
-   */
-  [[nodiscard]] virtual std::pair<std::size_t, std::size_t> do_get_mem_info(
-    cuda_stream_view stream) const
-  {
-    return {0, 0};
   }
 };
 static_assert(cuda::mr::async_resource_with<device_memory_resource, cuda::mr::device_accessible>);
