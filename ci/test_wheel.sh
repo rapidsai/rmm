b4aa83bd713c4f258d8cfe91ecc336f2f34ffa7c
ptaylor <paul.e.taylor@me.com>
2024-02-07 05:46:34 +0000

    make test_wheel.sh call run_pytests.sh

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -13,5 +13,6 @@ python -m pip install $(echo ./dist/rmm*.whl)[test]
 if [[ "$(arch)" == "aarch64" && ${RAPIDS_BUILD_TYPE} == "pull-request" ]]; then
     python ./ci/wheel_smoke_test.py
 else
-    python -m pytest ./python/rmm/tests
+    # Support invoking test_wheel.sh outside the script directory
+    "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/run_pytests.sh
 fi
