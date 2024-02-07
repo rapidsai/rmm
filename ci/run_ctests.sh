ee42ef49c382fc4524bf92b7f98d25a3e85c7a40
ptaylor <paul.e.taylor@me.com>
2024-02-07 17:05:19 +0000

    fail if gtests aren't installed

--- ci/run_ctests.sh
+++ ci/run_ctests.sh
@@ -3,8 +3,7 @@
 
 set -euo pipefail
 
-if [ -d "${INSTALL_PREFIX:-${CONDA_PREFIX:-/usr}}/bin/gtests/librmm/" ]; then
-    # Support customizing the ctests' install location
-    cd "${INSTALL_PREFIX:-${CONDA_PREFIX:-/usr}}/bin/gtests/librmm/"
-    ctest --no-tests=error --output-on-failure "$@"
-fi
+# Support customizing the ctests' install location
+cd "${INSTALL_PREFIX:-${CONDA_PREFIX:-/usr}}/bin/gtests/librmm/"
+
+ctest --no-tests=error --output-on-failure "$@"
