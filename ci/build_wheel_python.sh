3d0bb7d522726c8f73c8a58c4d24186496dd72e4
Vyas Ramasubramani <vyasr@nvidia.com>
2024-04-01 21:40:41 +0000

    Simplify name

--- ci/build_wheel_python.sh
+++ ci/build_wheel_python.sh
@@ -14,7 +14,7 @@ commit=$(git rev-parse HEAD)
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 # TODO: Need to update rapids-download-wheels-from-s3 to support a Python version-agnostic wheel
-RAPIDS_PY_WHEEL_NAME="librmm_${RAPIDS_PY_CUDA_SUFFIX}" RAPIDS_PY_VERSION="3.11" rapids-download-wheels-from-s3 /tmp/librmm_wheel_dist
+RAPIDS_PY_WHEEL_NAME="librmm_${RAPIDS_PY_CUDA_SUFFIX}" RAPIDS_PY_VERSION="3.11" rapids-download-wheels-from-s3 /tmp/librmm_dist
 
 # This is the version of the suffix with a preceding hyphen. It's used
 # everywhere except in the final wheel name.
@@ -38,7 +38,7 @@ fi
 
 cd "${package_dir}"
 
-PIP_FIND_LINKS="/tmp/librmm_wheel_dist" python -m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check
+PIP_FIND_LINKS="/tmp/librmm_dist" python -m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check
 
 mkdir -p final_dist
 python -m auditwheel repair -w final_dist dist/*
