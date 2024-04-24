f5cb0b721f848de2c5616e3b73d92081bc39262b
Mark Harris <783069+harrism@users.noreply.github.com>
2024-04-25 07:20:31 +1000

    os.path.join
    Co-authored-by: Vyas Ramasubramani <vyas.ramasubramani@gmail.com>

--- python/rmm/docs/conf.py
+++ python/rmm/docs/conf.py
@@ -95,7 +95,7 @@ suppress_warnings = []
 # if the file deprecated.xml does not exist in the doxygen xml output, breathe
 # will fail to build the docs, so we conditionally add "deprecated.rst" to the
 # exclude_patterns list
-if not os.path.exists(breathe_projects["librmm"] + "/deprecated.xml"):
+if not os.path.exists(os.path.join(breathe_projects["librmm"], "deprecated.xml")):
     exclude_patterns.append("librmm_docs/deprecated.rst")
     suppress_warnings.append("toc.excluded")
 
