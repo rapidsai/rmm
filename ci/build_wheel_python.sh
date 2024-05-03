352731c21b8dd3b5c1855d43845e926600fcbd3a
James Lamb <jlamb@nvidia.com>
2024-05-03 14:14:12 -0700

    adjust to presence of cpp wheels

--- ci/build_wheel_python.sh
+++ ci/build_wheel_python.sh
@@ -38,6 +38,7 @@ fi
 
 cd "${package_dir}"
 
+RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 CPP_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="rmm_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 cpp /tmp/librmm_dist)
 
 python -m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check --find-links "${CPP_WHEELHOUSE}"
@@ -45,5 +46,4 @@ python -m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check --find-
 mkdir -p final_dist
 python -m auditwheel repair -w final_dist dist/*
 
-RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 RAPIDS_PY_WHEEL_NAME="${package_name}_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 python final_dist
