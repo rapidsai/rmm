eed5625fb7603e9ce160bc4b296df27fa2099cb4
James Lamb <jlamb@nvidia.com>
2024-05-07 11:13:16 -0700

    get better logs, comment out conda jobs

--- ci/build_wheel_python.sh
+++ ci/build_wheel_python.sh
@@ -40,6 +40,9 @@ cd "${package_dir}"
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 CPP_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="rmm_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 cpp /tmp/librmm_dist)
+echo "---"
+ls /tmp/librmm_dist
+echo "---"
 
 python -m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check --find-links "${CPP_WHEELHOUSE}"
 
