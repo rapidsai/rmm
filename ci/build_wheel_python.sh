f57e124ad5576516cc88992171bbc8bcc68c1332
James Lamb <jlamb@nvidia.com>
2024-05-08 07:35:33 -0700

    uncomment more CI

--- ci/build_wheel_python.sh
+++ ci/build_wheel_python.sh
@@ -26,23 +26,16 @@ source rapids-date-string
 version=$(rapids-generate-version)
 echo "${version}" > VERSION
 
-if rapids-is-release-build; then
-    export RAPIDS_ALLOW_NIGHTLY_DEPS=0
-fi
-
-# Need to manually patch the cuda-python version for CUDA 12.
-ctk_major=$(echo "${RAPIDS_CUDA_VERSION}" | cut -d'.' -f1)
-if [[ ${ctk_major} == "12" ]]; then
-    sed -i "s/cuda-python[<=>\.,0-9a]*/cuda-python>=12.0,<13.0a0/g" ${package_dir}/pyproject.toml
-fi
+# # Need to manually patch the cuda-python version for CUDA 12.
+# ctk_major=$(echo "${RAPIDS_CUDA_VERSION}" | cut -d'.' -f1)
+# if [[ ${ctk_major} == "12" ]]; then
+#     sed -i "s/cuda-python[<=>\.,0-9a]*/cuda-python>=12.0,<13.0a0/g" ${package_dir}/pyproject.toml
+# fi
 
 cd "${package_dir}"
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 CPP_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="rmm_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 cpp /tmp/librmm_dist)
-echo "---"
-ls /tmp/librmm_dist
-echo "---"
 
 python -m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check --find-links "${CPP_WHEELHOUSE}"
 
