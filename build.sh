20c98e83f1dc157a2db2336020dc151bfe47fa1c
James Lamb <jlamb@nvidia.com>
2024-05-07 20:28:52 -0700

    fix rapids-build-backend installation

--- build.sh
+++ build.sh
@@ -156,17 +156,18 @@ if hasArg clean; then
 fi
 
 # TODO: remove before merging (when new rapids-build-backend is released)
-git clone \
-    -b main \
-    https://github.com/rapidsai/rapids-build-backend.git \
-    /tmp/delete-me/rapids-build-backend
+if [[ ! -d /tmp/delete-me/rapids-build-backend ]]; then
+    git clone \
+        -b main \
+        https://github.com/rapidsai/rapids-build-backend.git \
+        /tmp/delete-me/rapids-build-backend
 
-pushd /tmp/delete-me/rapids-build-backend
-sed -e 's/^version =.*/version = "0.0.2"/' -i pyproject.toml
-python -m pip install .
-popd
-
-export PIP_FIND_LINKS="file:///tmp/delete-me/rapids-build-backend"
+    pushd /tmp/delete-me/rapids-build-backend
+    sed -e 's/^version =.*/version = "0.0.2"/' -i pyproject.toml
+    python -m pip wheel --no-deps -w ./dist .
+    popd
+fi
+export PIP_FIND_LINKS="file:///tmp/delete-me/rapids-build-backend/dist"
 
 ################################################################################
 # Configure, build, and install librmm
--- ci/build_wheel_python.sh
+++ ci/build_wheel_python.sh
@@ -12,10 +12,10 @@ if [[ ! -d /tmp/delete-me/rapids-build-backend ]]; then
 
     pushd /tmp/delete-me/rapids-build-backend
     sed -e 's/^version =.*/version = "0.0.2"/' -i pyproject.toml
-    python -m pip install .
+    python -m pip wheel --no-deps -w ./dist .
     popd
 fi
-export PIP_FIND_LINKS="file:///tmp/delete-me/rapids-build-backend"
+export PIP_FIND_LINKS="file:///tmp/delete-me/rapids-build-backend/dist"
 
 package_name="rmm"
 package_dir="python/rmm"
