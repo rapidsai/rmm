58f418f11b0cd12de6685fa624ce80777e955350
James Lamb <jlamb@nvidia.com>
2024-05-09 13:35:34 -0700

    librmm wheel too

--- ci/build_wheel_cpp.sh
+++ ci/build_wheel_cpp.sh
@@ -3,26 +3,16 @@
 
 set -euo pipefail
 
-package_name="librmm"
 package_dir="python/librmm"
 
 source rapids-configure-sccache
 source rapids-date-string
 
 version=$(rapids-generate-version)
-commit=$(git rev-parse HEAD)
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 
-# This is the version of the suffix with a preceding hyphen. It's used
-# everywhere except in the final wheel name.
-PACKAGE_CUDA_SUFFIX="-${RAPIDS_PY_CUDA_SUFFIX}"
-
-pyproject_file="${package_dir}/pyproject.toml"
-
-sed -i "s/name = \"${package_name}\"/name = \"${package_name}${PACKAGE_CUDA_SUFFIX}\"/g" ${pyproject_file}
 echo "${version}" > VERSION
-sed -i "/^__git_commit__/ s/= .*/= \"${commit}\"/g" "${package_dir}/${package_name}/_version.py"
 
 cd "${package_dir}"
 
