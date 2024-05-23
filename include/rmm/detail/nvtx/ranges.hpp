4e9482f5a7473dd8b960f76c9c399576c9662ae3
Mark Harris <783069+harrism@users.noreply.github.com>
2024-05-23 10:00:38 +1000

    Add NVTX support and RMM_FUNC_RANGE() macro (#1558)
    Let's get RMM allocate/deallocates showing up in profiler timelines.
    
    Closes #495
    
    Authors:
      - Mark Harris (https://github.com/harrism)
      - Bradley Dice (https://github.com/bdice)
    
    Approvers:
      - Lawrence Mitchell (https://github.com/wence-)
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    URL: https://github.com/rapidsai/rmm/pull/1558

---
+++ include/rmm/detail/nvtx/ranges.hpp
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2024, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <nvtx3/nvtx3.hpp>
+
+namespace rmm {
+/**
+ * @brief Tag type for librmm's NVTX domain.
+ */
+struct librmm_domain {
+  static constexpr char const* name{"librmm"};  ///< Name of the librmm domain
+};
+
+/**
+ * @brief Alias for an NVTX range in the librmm domain.
+ *
+ * Customizes an NVTX range with the given input.
+ *
+ * Example:
+ * ```
+ * void some_function(){
+ *    rmm::scoped_range rng{"custom_name"}; // Customizes range name
+ *    ...
+ * }
+ * ```
+ */
+using scoped_range = ::nvtx3::scoped_range_in<librmm_domain>;
+
+}  // namespace rmm
+
+/**
+ * @brief Convenience macro for generating an NVTX range in the `librmm` domain
+ * from the lifetime of a function.
+ *
+ * Uses the name of the immediately enclosing function returned by `__func__` to
+ * name the range.
+ *
+ * Example:
+ * ```
+ * void some_function(){
+ *    RMM_FUNC_RANGE();
+ *    ...
+ * }
+ * ```
+ */
+#define RMM_FUNC_RANGE() NVTX3_FUNC_RANGE_IN(rmm::librmm_domain)
