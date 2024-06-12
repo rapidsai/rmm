e28e0c7cb48c80bc00cb9b65e650616a6148de29
James Lamb <jlamb@nvidia.com>
2024-06-12 09:34:21 -0500

    resolve dependency-file-generator warning, remove unnecessary rapids-build-backend configuration (#1582)
    Contributes to https://github.com/rapidsai/build-planning/issues/31
    Contributes to https://github.com/rapidsai/dependency-file-generator/issues/89
    
    #1502 was one of the first `rapids-build-backend` PRs merged across RAPIDS. Since it was merged, we've made some small adjustments to the approach for `rapids-build-backend`. This catches `rmm` up with those changes:
    
    * consolidates version-handling in `ci/build_cpp.sh`
    * removes `commit-file` configuration in `pyproject.toml`
      - *as of https://github.com/rapidsai/rapids-build-backend/pull/30, this is no longer necessary if the project's top-level directory is `{project_name}.replace("-", "_")*
      - *and anyway, it was changed from `commit-file` to `commit-files` in that PR, so `commit-file` was being silently ignored here*
    * uses `--file-key` instead of `--file_key` in `rapids-dependency-file-generator` calls
    
    Authors:
      - James Lamb (https://github.com/jameslamb)
    
    Approvers:
      - Kyle Edwards (https://github.com/KyleFromNVIDIA)
    
    URL: https://github.com/rapidsai/rmm/pull/1582

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2020-2023, NVIDIA CORPORATION.
+# Copyright (c) 2020-2024, NVIDIA CORPORATION.
 
 set -euo pipefail
 
@@ -13,11 +13,9 @@ export CMAKE_GENERATOR=Ninja
 
 rapids-print-env
 
-version=$(rapids-generate-version)
-
 rapids-logger "Begin cpp build"
 
 # This calls mambabuild when boa is installed (as is the case in the CI images)
-RAPIDS_PACKAGE_VERSION=${version} rapids-conda-retry mambabuild conda/recipes/librmm
+RAPIDS_PACKAGE_VERSION=$(rapids-generate-version) rapids-conda-retry mambabuild conda/recipes/librmm
 
 rapids-upload-conda-to-s3 cpp
