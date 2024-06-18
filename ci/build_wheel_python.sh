1a1ae19cd477f232f53f23568936ca4da301f0a1
James Lamb <jlamb@nvidia.com>
2024-06-18 10:38:56 -0700

    use constraints to enforce use of librmm produced in this CI run

--- ci/build_wheel_python.sh
+++ ci/build_wheel_python.sh
@@ -17,10 +17,9 @@ RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 CPP_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="rmm_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 cpp /tmp/librmm_dist)
 
 # ensure 'rmm' wheel builds always use the 'librmm' just built in the same CI run
-#echo "librmm-${RAPIDS_PY_CUDA_SUFFIX} @ file://$(echo ${CPP_WHEELHOUSE}/librmm_${RAPIDS_PY_CUDA_SUFFIX}*.whl)" > ./build-constraints.txt
-#python -m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check --constraints "${PWD}/build-constraints.txt"
+echo "librmm-${RAPIDS_PY_CUDA_SUFFIX} @ file://$(echo ${CPP_WHEELHOUSE}/librmm_${RAPIDS_PY_CUDA_SUFFIX}*.whl)" > ./build-constraints.txt
 
-python -m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check --find-links "${CPP_WHEELHOUSE}"
+python -m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check --constraints "${PWD}/build-constraints.txt"
 
 mkdir -p final_dist
 python -m auditwheel repair -w final_dist dist/*
