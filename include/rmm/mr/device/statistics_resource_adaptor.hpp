eb1695b2a42835ece9feee24c3574b6925e9c0d3
Mark Harris <mharris@nvidia.com>
2024-06-25 02:32:21 +0000

    whitespace

--- include/rmm/mr/device/statistics_resource_adaptor.hpp
+++ include/rmm/mr/device/statistics_resource_adaptor.hpp
@@ -110,7 +110,7 @@ class statistics_resource_adaptor final : public device_memory_resource {
       total += val.total;
     }
   };
-  
+
   /**
    * @brief Construct a new statistics resource adaptor using `upstream` to satisfy
    * allocation requests.
