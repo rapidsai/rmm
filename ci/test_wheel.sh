78f17bf144a08235a8b41bb77dd8e3936b2ce733
Sebastian Berg <sebastianb@nvidia.com>
2024-07-15 13:55:59 +0200

    Use oldest test dependencies from dependencies.yaml in wheel tests

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -6,8 +6,19 @@ set -eou pipefail
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 WHEELHOUSE="${PWD}/dist/"
 RAPIDS_PY_WHEEL_NAME="rmm_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 python "${WHEELHOUSE}"
+PIP_PACKAGE=$(echo "${WHEELHOUSE}"/rmm_"${RAPIDS_PY_CUDA_SUFFIX}"*.whl | head -n1)
 
-# echo to expand wildcard before adding '[extra]' requires for pip
-python -m pip install -v "$(echo "${WHEELHOUSE}"/rmm_${RAPIDS_PY_CUDA_SUFFIX}*.whl)[test]"
+# Use `package[test]` to install latest test dependencies or explicitly install oldest.
+if [[ $RAPIDS_DEPENDENCIES != "oldest" ]]; then
+  python -m pip install -v "${PIP_PACKAGE}[test]"
+else
+  rapids-dependency-file-generator \
+      --output requirements \
+      --file-key test_python \
+      --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION};dependencies=${RAPIDS_DEPENDENCIES}" \
+    | tee oldest-dependencies.txt
+
+  python -m pip install -v "$PIP_PACKAGE" -r oldest-dependencies.txt
+fi
 
 python -m pytest ./python/rmm/rmm/tests
