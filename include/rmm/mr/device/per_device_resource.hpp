c7d443d3c015677ddc8e25bfaf45380377174cf1
Mark Harris <783069+harrism@users.noreply.github.com>
2024-08-20 22:37:34 +1000

    Apply suggestions from code review
    Co-authored-by: Lawrence Mitchell <wence@gmx.li>

--- include/rmm/mr/device/per_device_resource.hpp
+++ include/rmm/mr/device/per_device_resource.hpp
@@ -305,7 +305,6 @@ inline device_async_resource_ref get_per_device_resource_ref(cuda_device_id devi
   // If a resource was never set for `id`, set to the initial resource
   auto const found = map.find(device_id.value());
   if (found == map.end()) {
-    //
     auto item = map.insert({device_id.value(), detail::initial_resource()});
     return item.first->second;
   }
@@ -342,11 +341,10 @@ inline device_async_resource_ref set_per_device_resource_ref(
   std::lock_guard<std::mutex> lock{detail::ref_map_lock()};
   auto& map          = detail::get_ref_map();
   auto const old_itr = map.find(device_id.value());
-  // If a resource didn't previously exist for `id`, return pointer to initial_resource
+  // If a resource didn't previously exist for `device_id`, return pointer to initial_resource
   // Note: because resource_ref is not default-constructible, we can't use std::map::operator[]
   if (old_itr == map.end()) {
     map.insert({device_id.value(), new_resource_ref});
-    std::cout << "returning initial resource in set_per_device_resource_ref\n";
     return device_async_resource_ref{detail::initial_resource()};
   }
 
