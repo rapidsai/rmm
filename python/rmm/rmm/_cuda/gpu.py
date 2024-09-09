47b1e53be436c3ce030cd5a12858d846976a16bd
Matthew Murray <matthewmurray711@gmail.com>
2024-09-09 16:22:41 -0700

    check status code, update docstring

--- python/rmm/rmm/_cuda/gpu.py
+++ python/rmm/rmm/_cuda/gpu.py
@@ -77,8 +77,14 @@ def runtimeGetVersion():
     Returns the version number of the current CUDA Runtime instance.
     The version is returned as (1000 major + 10 minor). For example,
     CUDA 9.2 would be represented by 9020.
+
+    This function automatically raises CUDARuntimeError with error message
+    and status code.
     """
-    return cudart.getLocalRuntimeVersion()[1]
+    status, version = cudart.getLocalRuntimeVersion()
+    if status != cudart.cudaError_t.cudaSuccess:
+        raise CUDARuntimeError(status)
+    return version
 
 
 def getDeviceCount():
