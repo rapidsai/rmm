724b96237a660e4ee67f3195d2cbdb1d002a4381
Matthew Murray <matthewmurray711@gmail.com>
2024-09-18 09:52:11 -0700

    replace lib with _logger

--- python/rmm/rmm/allocators/torch.py
+++ python/rmm/rmm/allocators/torch.py
@@ -28,10 +28,10 @@ else:
         # allocator .so relative to the current file because the current file
         # is pure Python and will therefore be in the source directory.
         # Instead, we search relative to an arbitrary file in the compiled
-        # package. We use the _lib.lib module because it is small.
-        from rmm.cpp import lib
+        # package. We use the cpp._logger module because it is small.
+        from rmm.cpp import _logger
 
-        sofile = pathlib.Path(lib.__file__).parent / "_torch_allocator.so"
+        sofile = pathlib.Path(_logger.__file__).parent / "_torch_allocator.so"
         rmm_torch_allocator = CUDAPluggableAllocator(
             str(sofile.absolute()),
             alloc_fn_name="allocate",
