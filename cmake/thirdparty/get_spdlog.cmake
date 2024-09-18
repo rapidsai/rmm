12eaa62255aa9f98c3cf8e54a20287cbc1a46969
James Lamb <jlamb@nvidia.com>
2024-09-18 10:28:06 -0700

    more exporting [skip ci]

--- cmake/thirdparty/get_spdlog.cmake
+++ cmake/thirdparty/get_spdlog.cmake
@@ -17,8 +17,10 @@
 function(find_and_configure_spdlog)
 
   include(${rapids-cmake-dir}/cpm/spdlog.cmake)
-  rapids_cpm_spdlog(FMT_OPTION "EXTERNAL_FMT_HO" INSTALL_EXPORT_SET rmm-exports)
-  rapids_export_package(BUILD spdlog rmm-exports)
+  rapids_cpm_spdlog(
+    FMT_OPTION "EXTERNAL_FMT_HO"
+    INSTALL_EXPORT_SET rmm-exports
+    BUILD_EXPORT_SET rmm-exports)
 
 endfunction()
 
