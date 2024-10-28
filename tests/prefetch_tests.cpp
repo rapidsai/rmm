809996607093336becc3de7b813e3c7123197fb4
Robert Maynard <rmaynard@nvidia.com>
2024-10-28 15:46:11 -0400

    Correct nvcc 12.4 compile errors with tests

--- tests/prefetch_tests.cpp
+++ tests/prefetch_tests.cpp
@@ -53,7 +53,7 @@ struct PrefetchTest : public ::testing::Test {
     // https://docs.nvidia.com/cuda/cuda-runtime-api/group__CUDART__MEMORY.html#group__CUDART__MEMORY_1g8048f6ea5ad77917444567656c140c5a
     // specifically for when cudaMemRangeAttribute::cudaMemRangeAttributeLastPrefetchLocation is
     // used.
-    constexpr size_t prefetch_data_size = 4;
+    [[maybe_unused]] constexpr size_t prefetch_data_size = 4;
     if constexpr (std::is_same_v<MemoryResourceType, rmm::mr::managed_memory_resource>) {
       int prefetch_location{0};
       RMM_CUDA_TRY(
