15cdb0cfa09c2a4231baad28eb389d07a176375d
Vyas Ramasubramani <vyasr@nvidia.com>
2024-11-11 00:14:04 +0000

    Make notes on what changes will be needed to use hide all spdlog symbols when we remove backwards compatibility mode.

--- cmake/thirdparty/get_spdlog.cmake
+++ cmake/thirdparty/get_spdlog.cmake
@@ -16,10 +16,21 @@
 function(find_and_configure_spdlog)
 
   include(${rapids-cmake-dir}/cpm/spdlog.cmake)
+  # TODO: When we disable backwards compatibility mode we will switch these flags on to build a
+  # static lib of spdlog and link to that. Forcing a download is one way to rebuild and ensure that
+  # we can build with the necessary symbol visibility flag. The other option is to pass
+  # Wl,--exclude-libs,libspdlog to the linker, which seems to capture a couple of symbols that this
+  # setting misses. However, in either case we may need to force building since spdlog's CMake
+  # reuses the same target for static and dynamic library builds. set(CPM_DOWNLOAD_spdlog ON)
+  # set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fvisibility=hidden -fvisibility-inlines-hidden")
   rapids_cpm_spdlog(
     FMT_OPTION "EXTERNAL_FMT_HO"
     INSTALL_EXPORT_SET rmm-exports
-    BUILD_EXPORT_SET rmm-exports)
+    BUILD_EXPORT_SET rmm-exports
+    # TODO: We can't support both modes right now unfortunately because we require a static spdlog
+    # for the new builds to hide the symbols properly. CPM_ARGS OPTIONS "SPDLOG_BUILD_SHARED OFF"
+    # "BUILD_SHARED_LIBS OFF"
+  )
 
 endfunction()
 
