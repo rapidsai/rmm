cd71f30544fffeb4f446a795361ba1366ea92f20
Vyas Ramasubramani <vyasr@nvidia.com>
2024-11-17 18:13:36 +0000

    Enable dynamic linking to spdlog

--- cmake/thirdparty/get_spdlog.cmake
+++ cmake/thirdparty/get_spdlog.cmake
@@ -12,26 +12,30 @@
 # the License.
 # =============================================================================
 
-# Use CPM to find or clone speedlog
+# Use CPM to find or clone speedlog TODO: The logic here should be upstreamed into rapids-logger and
+# probably removed from rapids-cmake. Note that it is not possible to build with two different modes
+# for different projects in the same build environment, so this functionality must be kept
+# independent of the rapids_make_logger function. Alternatively, we could update the cpm calls for
+# spdlog to not promote targets to global scope and instead allow each project to find and configure
+# spdlog independently.
 function(find_and_configure_spdlog)
 
   include(${rapids-cmake-dir}/cpm/spdlog.cmake)
-  set(_options)
-  # TODO: Figure out how to use this variable properly inside rapids_cpm_spdlog. The way quotes are
-  # being interpolated is almost certainly not what I expect. set(_options OPTIONS
-  # "SPDLOG_BUILD_SHARED OFF" "BUILD_SHARED_LIBS OFF")
   if(RMM_SPDLOG_DYNAMIC)
-    # Has no effect as of yet.
-    set(_options OPTIONS "SPDLOG_BUILD_SHARED ON" "BUILD_SHARED_LIBS ON")
+    rapids_cpm_spdlog(
+      INSTALL_EXPORT_SET rmm-exports BUILD_EXPORT_SET rmm-exports OPTIONS "SPDLOG_BUILD_SHARED ON"
+                                                                  "BUILD_SHARED_LIBS ON")
   else()
+    # For static spdlog usage assume that we want to hide as many symbols as possible, so do not use
+    # pre-built libraries. It's quick enough to build that there is no real benefit to supporting
+    # the alternative.
     set(CPM_DOWNLOAD_spdlog ON)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fvisibility=hidden -fvisibility-inlines-hidden")
+    rapids_cpm_spdlog(
+      INSTALL_EXPORT_SET rmm-exports
+      BUILD_EXPORT_SET rmm-exports OPTIONS "SPDLOG_BUILD_SHARED OFF" "BUILD_SHARED_LIBS OFF"
+                                   EXCLUDE_FROM_ALL)
   endif()
-  rapids_cpm_spdlog(
-    INSTALL_EXPORT_SET rmm-exports
-    BUILD_EXPORT_SET rmm-exports OPTIONS "SPDLOG_BUILD_SHARED OFF" "BUILD_SHARED_LIBS OFF"
-                                 EXCLUDE_FROM_ALL)
-
 endfunction()
 
 find_and_configure_spdlog()
