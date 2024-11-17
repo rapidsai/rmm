dd906e09a1dea506c44a4919a1aae8eaa4057b92
Vyas Ramasubramani <vyasr@nvidia.com>
2024-11-17 19:58:23 +0000

    Switch to using bundled fmt

--- cmake/thirdparty/get_spdlog.cmake
+++ cmake/thirdparty/get_spdlog.cmake
@@ -23,14 +23,19 @@ function(find_and_configure_spdlog)
   include(${rapids-cmake-dir}/cpm/spdlog.cmake)
   if(RMM_SPDLOG_DYNAMIC)
     rapids_cpm_spdlog(
-      INSTALL_EXPORT_SET rmm-exports BUILD_EXPORT_SET rmm-exports OPTIONS "SPDLOG_BUILD_SHARED ON"
-                                                                  "BUILD_SHARED_LIBS ON")
+      FMT_OPTION "BUNDLED"
+      INSTALL_EXPORT_SET rmm-exports
+      BUILD_EXPORT_SET rmm-exports OPTIONS "SPDLOG_BUILD_SHARED ON" "BUILD_SHARED_LIBS ON")
   else()
     # For static spdlog usage assume that we want to hide as many symbols as possible, so do not use
     # pre-built libraries. It's quick enough to build that there is no real benefit to supporting
     # the alternative.
     set(CPM_DOWNLOAD_spdlog ON)
     rapids_cpm_spdlog(
+      # TODO: Is this safe to set up for all projects? Do we have to worry about the fmt patch
+      # currently in rapids-cmake? We should never be compiling files using spdlog under nvcc
+      # anymore.
+      FMT_OPTION "BUNDLED"
       INSTALL_EXPORT_SET rmm-exports
       BUILD_EXPORT_SET rmm-exports OPTIONS "SPDLOG_BUILD_SHARED OFF" "BUILD_SHARED_LIBS OFF"
                                    EXCLUDE_FROM_ALL)
