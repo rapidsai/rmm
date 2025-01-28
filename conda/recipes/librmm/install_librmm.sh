17f9f13e6506874dc60db4406fa0083360ad8a78
Gil Forsyth <gforsyth@nvidia.com>
2025-01-28 15:20:11 -0500

    builds, but compiles for each output which is bad

--- conda/recipes/librmm/install_librmm.sh
+++ conda/recipes/librmm/install_librmm.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+
+./build.sh -n -v clean librmm tests benchmarks --cmake-args=\"-DCMAKE_INSTALL_LIBDIR=lib\"
+
 cmake --install build
--- conda/recipes/librmm/install_librmm_tests.sh
+++ conda/recipes/librmm/install_librmm_tests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+./build.sh -n -v clean librmm tests benchmarks --cmake-args=\"-DCMAKE_INSTALL_LIBDIR=lib\"
 cmake --install build --component testing
