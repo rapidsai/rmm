cdb334d0b807523f806d05d66c5e0d94bf16dafb
Gil Forsyth <gforsyth@nvidia.com>
2025-02-05 08:48:06 -0500

    Remove manual env-var setting and rattler install

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -19,25 +19,13 @@ sccache --zero-stats
 RAPIDS_PACKAGE_VERSION=$(rapids-generate-version)
 export RAPIDS_PACKAGE_VERSION
 
-# rattler-build doesn't have built-in support for GIT_DESCRIBE_NUMBER and
-# GIT_DESCRIBE_HASH so we set them in the environment first
-# shellcheck disable=SC2034
-IFS=- read -r TAG_VERSION GIT_DESCRIBE_NUMBER GIT_DESCRIBE_HASH <<< "$(git describe --tags)"
-unset TAG_VERSION
-export GIT_DESCRIBE_NUMBER
-export GIT_DESCRIBE_HASH
-
-mamba install rattler-build -c conda-forge -y
+source rapids-configure-rattler
 
 rattler-build build --recipe conda/recipes/librmm \
                     --experimental \
                     --no-build-id \
                     --channel-priority disabled \
                     --output-dir "$RAPIDS_CONDA_BLD_OUTPUT_DIR"
-                    # These are probably set via `rapids-configure-conda-channels`
-                    # -c rapidsai \
-                    # -c conda-forge \
-                    # -c nvidia
 
 sccache --show-adv-stats
 
--- ci/build_python.sh
+++ ci/build_python.sh
@@ -23,14 +23,7 @@ sccache --zero-stats
 RAPIDS_PACKAGE_VERSION=$(head -1 ./VERSION)
 export RAPIDS_PACKAGE_VERSION
 
-# rattler-build doesn't have built-in support for GIT_DESCRIBE_NUMBER and
-# GIT_DESCRIBE_HASH so we set them in the environment first
-# shellcheck disable=SC2034
-IFS=- read -r GIT_DESCRIBE_NUMBER GIT_DESCRIBE_NUMBER GIT_DESCRIBE_HASH <<< "$(git describe --tags)"
-export GIT_DESCRIBE_NUMBER
-export GIT_DESCRIBE_HASH
-
-mamba install rattler-build -c conda-forge -y
+source rapids-configure-rattler
 
 rattler-build build --recipe conda/recipes/rmm \
                     --experimental \
