631bb53ecd7081bd75d6dfff2492f59abaaf3b10
Bradley Dice <bdice@bradleydice.com>
2025-02-24 17:24:31 -0600

    Remove set_current_device_resource.

--- examples/basic/src/basic.cpp
+++ examples/basic/src/basic.cpp
@@ -18,7 +18,6 @@
 #include <rmm/cuda_stream.hpp>
 #include <rmm/device_uvector.hpp>
 #include <rmm/mr/device/cuda_async_memory_resource.hpp>
-#include <rmm/mr/device/device_memory_resource.hpp>
 
 int main(int argc, char** argv)
 {
@@ -27,19 +26,12 @@ int main(int argc, char** argv)
   // available GPU memory.
   rmm::mr::cuda_async_memory_resource mr{rmm::percent_of_free_device_memory(50)};
 
-  // Set the async resource to be used by default for all device memory
-  // allocations. Note: It is the user's responsibility to ensure the `mr`
-  // object stays alive for the duration of it being set as the default.
-  // Call this before the first allocation to ensure all data is allocated by
-  // the same memory resource.
-  rmm::mr::set_current_device_resource(&mr);
-
   // Create a CUDA stream for asynchronous allocations
   auto stream = rmm::cuda_stream{};
 
-  // Create a device_uvector on this stream
+  // Create a device_uvector with this stream and memory resource
   auto const size{12345};
-  rmm::device_uvector<int> vec(size, stream);
+  rmm::device_uvector<int> vec(size, stream, mr);
   std::cout << "vec size: " << vec.size() << std::endl;
 
   // Synchronize the stream
