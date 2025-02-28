844464b404cdbe8c2a3b86954e2ca1981bf05270
Mike Sarahan <msarahan@nvidia.com>
2025-02-28 09:24:57 -0600

    fix GITHUB_WORKSPACE not being present locally (#1841)
    #1839 introduced a hard dependence on GITHUB_WORKSPACE being defined. That is not true outside of github actions. This PR fixes it to fall back to the current folder.
    
    Authors:
      - Mike Sarahan (https://github.com/msarahan)
      - Bradley Dice (https://github.com/bdice)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/rmm/pull/1841

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -28,14 +28,15 @@ source rapids-telemetry-setup
 # --no-build-id allows for caching with `sccache`
 # more info is available at
 # https://rattler.build/latest/tips_and_tricks/#using-sccache-or-ccache-with-rattler-build
-rattler-build build --recipe conda/recipes/librmm \
-                    --experimental \
-                    --no-build-id \
-                    --channel-priority disabled \
-                    --output-dir "$RAPIDS_CONDA_BLD_OUTPUT_DIR" \
-                    "${RATTLER_CHANNELS[@]}" 2>&1 | tee ${GITHUB_WORKSPACE}/telemetry-artifacts/build.log
+rapids-telemetry-record build.log rattler-build build \
+    --recipe conda/recipes/librmm \
+    --experimental \
+    --no-build-id \
+    --channel-priority disabled \
+    --output-dir "$RAPIDS_CONDA_BLD_OUTPUT_DIR" \
+    "${RATTLER_CHANNELS[@]}"
 
-sccache --show-adv-stats | tee ${GITHUB_WORKSPACE}/telemetry-artifacts/sccache-stats.txt
+rapids-telemetry-record sccache-stats.txt sccache --show-adv-stats
 
 # remove build_cache directory
 rm -rf "$RAPIDS_CONDA_BLD_OUTPUT_DIR"/build_cache
--- ci/build_wheel_cpp.sh
+++ ci/build_wheel_cpp.sh
@@ -19,9 +19,9 @@ sccache --zero-stats
 # Creates artifacts directory for telemetry
 source rapids-telemetry-setup
 
-rapids-pip-retry wheel . -w dist -v --no-deps --disable-pip-version-check 2>&1 | tee ${GITHUB_WORKSPACE}/telemetry-artifacts/build.log
+rapids-telemetry-record build.log rapids-pip-retry wheel . -w dist -v --no-deps --disable-pip-version-check
 
-sccache --show-adv-stats | tee ${GITHUB_WORKSPACE}/telemetry-artifacts/sccache-stats.txt
+rapids-telemetry-record sccache-stats.txt sccache --show-adv-stats
 
 python -m wheel tags --platform any dist/* --remove
 
