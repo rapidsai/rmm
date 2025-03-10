cd7d08b37518e7f54ebd0a9e234d8c4e6de40df0
Bradley Dice <bdice@bradleydice.com>
2025-03-10 07:49:16 -0500

    Apply suggestions from code review
    Co-authored-by: Lawrence Mitchell <wence@gmx.li>

--- tests/libcudacxx_flag_test/libcudacxx_flag_test.cpp
+++ tests/libcudacxx_flag_test/libcudacxx_flag_test.cpp
@@ -16,15 +16,15 @@
 
 /**
  * @file libcudacxx_flag_test.cpp
- * @brief Test that verifies the error message when LIBCUDACXX_ENABLE_EXPERIMENTAL_MEMORY_RESOURCE
+ * @brief Test that verifies the error message when `LIBCUDACXX_ENABLE_EXPERIMENTAL_MEMORY_RESOURCE`
  * is not defined.
  *
  * This test is expected to fail to compile with a clear error message.
  * To run this test, you need to compile it separately without defining
- * LIBCUDACXX_ENABLE_EXPERIMENTAL_MEMORY_RESOURCE.
+ * `LIBCUDACXX_ENABLE_EXPERIMENTAL_MEMORY_RESOURCE`.
  *
  * Example:
- * g++ -std=c++17 -I../../include libcudacxx_flag_test.cpp
+ * `g++ -std=c++17 -I../../include libcudacxx_flag_test.cpp`
  */
 
 // Include a header that requires LIBCUDACXX_ENABLE_EXPERIMENTAL_MEMORY_RESOURCE
