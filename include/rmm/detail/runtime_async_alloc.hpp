7708928870ca2f308d37aef4e4589b71e40f24c7
Bradley Dice <bdice@bradleydice.com>
2025-04-07 11:34:30 -0500

    clang-format

--- include/rmm/detail/runtime_async_alloc.hpp
+++ include/rmm/detail/runtime_async_alloc.hpp
@@ -91,9 +91,9 @@ struct runtime_async_alloc {
 #if defined(CUDA_VERSION) && CUDA_VERSION >= RMM_MIN_HWDECOMPRESS_CUDA_DRIVER_VERSION
     // Check if hardware decompression is supported (requires CUDA 12.8 driver or higher)
     static bool is_supported = []() {
-        int driver_version{};
-        RMM_CUDA_TRY(cudaDriverGetVersion(&driver_version));
-        return driver_version >= RMM_MIN_HWDECOMPRESS_CUDA_DRIVER_VERSION;
+      int driver_version{};
+      RMM_CUDA_TRY(cudaDriverGetVersion(&driver_version));
+      return driver_version >= RMM_MIN_HWDECOMPRESS_CUDA_DRIVER_VERSION;
     }();
     return is_supported;
 #else
