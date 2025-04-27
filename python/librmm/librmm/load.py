d22cd09d4645894fc211a3a69bac6e6e6fa12747
Bradley Dice <bdice@bradleydice.com>
2025-04-27 14:14:34 -0500

    Update build scripts

--- python/librmm/librmm/load.py
+++ python/librmm/librmm/load.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025, NVIDIA CORPORATION.
+# Copyright (c) 2024-2025, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,13 +13,82 @@
 # limitations under the License.
 #
 
+import ctypes
+import os
+
+# Loading with RTLD_LOCAL adds the library itself to the loader's
+# loaded library cache without loading any symbols into the global
+# namespace. This allows libraries that express a dependency on
+# this library to be loaded later and successfully satisfy this dependency
+# without polluting the global symbol table with symbols from
+# librmm that could conflict with symbols from other DSOs.
+PREFERRED_LOAD_FLAG = ctypes.RTLD_LOCAL
+
+
+def _load_system_installation(soname: str):
+    """Try to dlopen() the library indicated by ``soname``
+    Raises ``OSError`` if library cannot be loaded.
+    """
+    return ctypes.CDLL(soname, PREFERRED_LOAD_FLAG)
+
+
+def _load_wheel_installation(soname: str):
+    """Try to dlopen() the library indicated by ``soname``
+
+    Returns ``None`` if the library cannot be loaded.
+    """
+    if os.path.isfile(
+        lib := os.path.join(os.path.dirname(__file__), "lib64", soname)
+    ):
+        return ctypes.CDLL(lib, PREFERRED_LOAD_FLAG)
+    return None
+
 
 def load_library():
-    """Librmm is header-only, this function loads its (non-header-only) dependencies"""
+    """Dynamically load librmm.so and its dependencies"""
     try:
+        # rapids-logger must be loaded before librmm because librmm references
+        # it.
         import rapids_logger
-    except ModuleNotFoundError:
-        pass
-    else:
+
         rapids_logger.load_library()
-        del rapids_logger
+    except ModuleNotFoundError:
+        # librmm's runtime dependency on rapids-logger may be satisfied by a
+        # natively installed library or a conda package, in which case the
+        # import will fail and we assume the library is discoverable on system
+        # paths.
+        pass
+
+    prefer_system_installation = (
+        os.getenv("RAPIDS_LIBRMM_PREFER_SYSTEM_LIBRARY", "false").lower()
+        != "false"
+    )
+
+    soname = "librmm.so"
+    librmm_lib = None
+    if prefer_system_installation:
+        # Prefer a system library if one is present to
+        # avoid clobbering symbols that other packages might expect, but if no
+        # other library is present use the one in the wheel.
+        try:
+            librmm_lib = _load_system_installation(soname)
+        except OSError:
+            librmm_lib = _load_wheel_installation(soname)
+    else:
+        # Prefer the libraries bundled in this package. If they aren't found
+        # (which might be the case in builds where the library was prebuilt before
+        # packaging the wheel), look for a system installation.
+        try:
+            librmm_lib = _load_wheel_installation(soname)
+            if librmm_lib is None:
+                librmm_lib = _load_system_installation(soname)
+        except OSError:
+            # If none of the searches above succeed, just silently return None
+            # and rely on other mechanisms (like RPATHs on other DSOs) to
+            # help the loader find the library.
+            pass
+
+    # The caller almost never needs to do anything with this library, but no
+    # harm in offering the option since this object at least provides a handle
+    # to inspect where librmm was loaded from.
+    return librmm_lib
