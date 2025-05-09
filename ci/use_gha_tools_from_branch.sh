4c6bf108f5ff1c6748ea67d1a3578c14c0386de6
James Lamb <jlamb@nvidia.com>
2025-05-09 14:21:31 -0500

    change branch

--- ci/use_gha_tools_from_branch.sh
+++ ci/use_gha_tools_from_branch.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2025, NVIDIA CORPORATION.
 
 git clone \
-  --branch "gh-artifacts/local-repro" \
+  --branch "gh-artifacts/consolidate-scripts" \
   https://github.com/rapidsai/gha-tools.git \
   /tmp/gha-tools
 
