45325b1a056bd7b190e92dae1547ea4b05afeaa5
James Lamb <jlamb@nvidia.com>
2025-05-20 16:18:55 -0500

    update comment

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -12,7 +12,12 @@ RMM_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="rmm_${RAPIDS_PY_CUDA_SUFFIX}" rapids-down
 # generate constraints (possibly pinning to oldest support versions of dependencies)
 rapids-generate-pip-constraints test_python ./constraints.txt
 
-# echo to expand wildcard before adding '[extra]' requires for pip
+# notes:
+#
+#   * echo to expand wildcard before adding `[test]` requires for pip
+#   * need to provide --constraint="${PIP_CONSTRAINT}" because that environment variable is
+#     ignored if any other --constraint are passed via the CLI
+#
 rapids-pip-retry install \
     -v \
     --constraint ./constraints.txt \
