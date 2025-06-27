d07133fd776e1118e9aa419729f00f47414742ab
James Lamb <jlamb@nvidia.com>
2025-06-27 15:21:05 -0500

    test changes to rapids-github-run-id (#1972)
    Testing the changes from https://github.com/rapidsai/gha-tools/pull/196, which contribute to https://github.com/rapidsai/shared-workflows/issues/377
    
    I'm proposing that we **merge this as-is**, to test that these changes work in the following situations on `main`:
    
    * `branch` build triggered by a merge
    * manually-triggered `nightly` test run
    
    Then merge a follow-up PR reverting all of this, after https://github.com/rapidsai/gha-tools/pull/196 is merged.
    
    Authors:
      - James Lamb (https://github.com/jameslamb)
    
    Approvers:
      - Kyle Edwards (https://github.com/KyleFromNVIDIA)
    
    URL: https://github.com/rapidsai/rmm/pull/1972

--- ci/test_cpp.sh
+++ ci/test_cpp.sh
@@ -3,6 +3,8 @@
 
 set -euo pipefail
 
+source ./ci/use_gha_tools_from_branch.sh
+
 # Support invoking test_cpp.sh outside the script directory
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../
 
