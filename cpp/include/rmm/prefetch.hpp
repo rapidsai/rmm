51b548e3916e9e497e50331703141dfb85664474
Bradley Dice <bdice@bradleydice.com>
2025-07-09 08:04:51 -0500

    Precompile device_buffer

--- cpp/include/rmm/prefetch.hpp
+++ cpp/include/rmm/prefetch.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, NVIDIA CORPORATION.
+ * Copyright (c) 2024-2025, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@
 
 #include <rmm/cuda_device.hpp>
 #include <rmm/cuda_stream_view.hpp>
+#include <rmm/detail/error.hpp>
 #include <rmm/detail/export.hpp>
 #include <rmm/error.hpp>
 
