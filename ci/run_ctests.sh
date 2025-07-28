c7f17caeaa071443981fe6e7c8c98c877df36800
Bradley Dice <bdice@bradleydice.com>
2025-07-28 18:01:48 -0500

    Run all tests

--- ci/run_ctests.sh
+++ ci/run_ctests.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-# Copyright (c) 2024-2025, NVIDIA CORPORATION.
+# Copyright (c) 2024, NVIDIA CORPORATION.
 
 set -euo pipefail
 
 # Support customizing the ctests' install location
 cd "${INSTALL_PREFIX:-${CONDA_PREFIX:-/usr}}/bin/gtests/librmm/"
 
-# Run gtest verbosely
-./DEVICE_MR_REF_TEST --gtest_filter="ResourceTests/mr_ref_test.SetCurrentDeviceResourceRef/System"
-
-#ctest --no-tests=error --output-on-failure "$@"
+ctest --no-tests=error --output-on-failure "$@"
