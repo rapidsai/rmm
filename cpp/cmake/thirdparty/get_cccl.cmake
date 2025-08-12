4be752fb580c680516336f1260b16c08c99d860e
Bradley Dice <bdice@bradleydice.com>
2025-08-12 14:29:44 -0500

    Test with CCCL 3.1.0

--- cpp/cmake/thirdparty/get_cccl.cmake
+++ cpp/cmake/thirdparty/get_cccl.cmake
@@ -1,5 +1,5 @@
 # =============================================================================
-# Copyright (c) 2023, NVIDIA CORPORATION.
+# Copyright (c) 2023-2025, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
 # in compliance with the License. You may obtain a copy of the License at
@@ -16,6 +16,11 @@
 function(find_and_configure_cccl)
 
   include(${rapids-cmake-dir}/cpm/cccl.cmake)
+  include(${rapids-cmake-dir}/cpm/package_override.cmake)
+
+  set(rmm_patch_dir "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/patches")
+  rapids_cpm_package_override("${rmm_patch_dir}/cccl_override.json")
+
   rapids_cpm_cccl(BUILD_EXPORT_SET rmm-exports INSTALL_EXPORT_SET rmm-exports)
 
 endfunction()
