556375ac45c483cbf45343e053257b5f5dc0ac06
Lawrence Mitchell <lmitchell@nvidia.com>
2025-09-16 09:51:33 +0000

    Allow creation of stream pool with flags

--- cpp/src/cuda_stream_pool.cpp
+++ cpp/src/cuda_stream_pool.cpp
@@ -18,13 +18,17 @@
 #include <rmm/cuda_stream_view.hpp>
 #include <rmm/detail/error.hpp>
 
+#include <algorithm>
 #include <cstddef>
 
 namespace rmm {
 
-cuda_stream_pool::cuda_stream_pool(std::size_t pool_size) : streams_(pool_size)
+cuda_stream_pool::cuda_stream_pool(std::size_t pool_size, cuda_stream::flags flags)
 {
   RMM_EXPECTS(pool_size > 0, "Stream pool size must be greater than zero");
+  streams_.reserve(pool_size);
+  std::generate_n(
+    std::back_inserter(streams_), pool_size, [flags]() { return cuda_stream(flags); });
 }
 
 rmm::cuda_stream_view cuda_stream_pool::get_stream() const noexcept
