d40c71de10e1d49a50ca10af89b86b71b2c9c243
Bradley Dice <bdice@bradleydice.com>
2025-10-06 22:57:36 -0500

    Disable tests on CUDA < 13.0

--- python/rmm/rmm/tests/test_rmm.py
+++ python/rmm/rmm/tests/test_rmm.py
@@ -42,6 +42,8 @@ _IS_INTEGRATED_MEMORY_SYSTEM = rmm._cuda.gpu.getDeviceAttribute(
     runtime.cudaDeviceAttr.cudaDevAttrIntegrated, rmm._cuda.gpu.getDevice()
 )
 
+_ASYNC_MANAGED_MEMORY_SUPPORTED = rmm._cuda.gpu.runtimeGetVersion() >= 13000
+
 
 def array_tester(dtype, nelem, alloc):
     # data
@@ -736,6 +738,10 @@ def test_cuda_async_memory_resource_threshold(nelem, alloc):
     array_tester("u1", 2 * nelem, alloc)  # should trigger release
 
 
+@pytest.mark.skipif(
+    not _ASYNC_MANAGED_MEMORY_SUPPORTED,
+    reason="CudaAsyncManagedMemoryResource requires CUDA 13.0+",
+)
 @pytest.mark.parametrize("dtype", _dtypes)
 @pytest.mark.parametrize("nelem", _nelems)
 @pytest.mark.parametrize("alloc", _allocs)
@@ -746,6 +752,10 @@ def test_cuda_async_managed_memory_resource(dtype, nelem, alloc):
     array_tester(dtype, nelem, alloc)
 
 
+@pytest.mark.skipif(
+    not _ASYNC_MANAGED_MEMORY_SUPPORTED,
+    reason="CudaAsyncManagedMemoryResource requires CUDA 13.0+",
+)
 @pytest.mark.parametrize("nelems", _nelems)
 def test_cuda_async_managed_memory_resource_stream(nelems):
     mr = rmm.mr.experimental.CudaAsyncManagedMemoryResource()
@@ -757,6 +767,10 @@ def test_cuda_async_managed_memory_resource_stream(nelems):
     np.testing.assert_equal(expected, result)
 
 
+@pytest.mark.skipif(
+    not _ASYNC_MANAGED_MEMORY_SUPPORTED,
+    reason="CudaAsyncManagedMemoryResource requires CUDA 13.0+",
+)
 def test_cuda_async_managed_memory_resource_pool_handle():
     mr = rmm.mr.experimental.CudaAsyncManagedMemoryResource()
     pool_handle = mr.pool_handle()
