76f2259e1765b09bd28e6c57e51fcc19223bc653
Bradley Dice <bdice@bradleydice.com>
2025-10-08 18:03:48 -0500

    Add more static checks

--- cpp/include/rmm/mr/device/cuda_async_managed_memory_resource.hpp
+++ cpp/include/rmm/mr/device/cuda_async_managed_memory_resource.hpp
@@ -130,6 +130,8 @@ class cuda_async_managed_memory_resource final : public device_memory_resource {
 };
 
 // static property checks
+static_assert(rmm::detail::polyfill::resource<cuda_async_managed_memory_resource>);
+static_assert(rmm::detail::polyfill::async_resource<cuda_async_managed_memory_resource>);
 static_assert(rmm::detail::polyfill::resource_with<cuda_async_managed_memory_resource,
                                                    cuda::mr::device_accessible>);
 static_assert(rmm::detail::polyfill::async_resource_with<cuda_async_managed_memory_resource,
