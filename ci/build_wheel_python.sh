2b269be98937eb5d9af32efec9088b325719278e
Gil Forsyth <gforsyth@nvidia.com>
2025-12-15 13:19:44 -0500

    refactor(limited API): build wheels and conda packages using Python limited API

--- ci/build_wheel_python.sh
+++ ci/build_wheel_python.sh
@@ -64,3 +64,6 @@ absolute_wheel_dir=$(realpath "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}")
 # switch back to the root of the repo and check symbol visibility
 popd
 ci/check_symbols.sh "$(echo "${absolute_wheel_dir}"/rmm_*.whl)"
+
+RAPIDS_PACKAGE_NAME="$(rapids-package-name wheel_python rmm --stable --cuda)"
+export RAPIDS_PACKAGE_NAME
