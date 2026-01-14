8c2f022632e8c889b000582a2d6504d07d8ec303
Mike McCarty <mmccarty@nvidia.com>
2026-01-14 16:26:17 -0500

    Avoiding conflict with build-constraint and no-build-isolation

--- ci/build_wheel_python.sh
+++ ci/build_wheel_python.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: Copyright (c) 2023-2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2023-2026, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 
 set -euo pipefail
@@ -22,8 +22,8 @@ LIBRMM_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="librmm_${RAPIDS_PY_CUDA_SUFFIX}" rapid
 
 # ensure 'rmm' wheel builds always use the 'librmm' just built in the same CI run
 #
-# Using env variable PIP_CONSTRAINT (initialized by 'rapids-init-pip') is necessary to ensure the constraints
-# are used when creating the isolated build environment.
+# env variable 'PIP_CONSTRAINT' is set up by rapids-init-pip. It constrains all subsequent
+# 'pip install', 'pip download', etc. calls (except those used in 'pip wheel', handled separately in build scripts)
 echo "librmm-${RAPIDS_PY_CUDA_SUFFIX} @ file://$(echo "${LIBRMM_WHEELHOUSE}"/librmm_"${RAPIDS_PY_CUDA_SUFFIX}"*.whl)" >> "${PIP_CONSTRAINT}"
 
 sccache --stop-server 2>/dev/null || true
@@ -31,11 +31,24 @@ sccache --stop-server 2>/dev/null || true
 # Creates artifacts directory for telemetry
 source rapids-telemetry-setup
 
+RAPIDS_PIP_WHEEL_ARGS=(
+  -w dist
+  -v
+  --no-deps
+  --disable-pip-version-check
+)
+
+# Only use --build-constraint when build isolation is enabled.
+#
+# Passing '--build-constraint' and '--no-build-isolation` together results in an error from 'pip',
+# but we want to keep environment variable PIP_CONSTRAINT set unconditionally.
+# PIP_NO_BUILD_ISOLATION=0 means "add --no-build-isolation" (ref: https://github.com/pypa/pip/issues/573
+if [[ "${PIP_NO_BUILD_ISOLATION:-}" != "0" ]]; then
+    RAPIDS_PIP_WHEEL_ARGS+=(--build-constraint="${PIP_CONSTRAINT}")
+fi
+
 rapids-telemetry-record build.log rapids-pip-retry wheel \
-  -v \
-  -w dist \
-  --no-deps \
-  --disable-pip-version-check \
+  "${RAPIDS_PIP_WHEEL_ARGS[@]}" \
   .
 
 rapids-telemetry-record sccache-stats.txt sccache --show-adv-stats
