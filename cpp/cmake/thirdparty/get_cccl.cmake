e54cdf723553a07467cf4ed79ccc9233f95204e2
Bradley Dice <bdice@bradleydice.com>
2026-01-26 18:24:07 -0600

    Use CCCL 3.2.1 to get accessors for shared_resource.

--- cpp/cmake/thirdparty/get_cccl.cmake
+++ cpp/cmake/thirdparty/get_cccl.cmake
@@ -1,6 +1,6 @@
 # =============================================================================
 # cmake-format: off
-# SPDX-FileCopyrightText: Copyright (c) 2023-2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2023-2026, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 # cmake-format: on
 # =============================================================================
@@ -9,6 +9,11 @@
 function(find_and_configure_cccl)
 
   include(${rapids-cmake-dir}/cpm/cccl.cmake)
+  include(${rapids-cmake-dir}/cpm/package_override.cmake)
+
+  set(rmm_patch_dir "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/patches")
+  rapids_cpm_package_override("${rmm_patch_dir}/cccl_override.json")
+
   rapids_cpm_cccl(BUILD_EXPORT_SET rmm-exports INSTALL_EXPORT_SET rmm-exports)
 
 endfunction()
