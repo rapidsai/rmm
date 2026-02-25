e590fe17f95b799643a6f35cf811b29deae9c41d
Bradley Dice <bdice@bradleydice.com>
2026-02-25 00:42:58 +0000

    Work around breathe#916 friend constexpr friend bug via RMM_CONSTEXPR_FRIEND

--- cpp/include/rmm/mr/detail/pool_memory_resource_impl.hpp
+++ cpp/include/rmm/mr/detail/pool_memory_resource_impl.hpp
@@ -53,7 +53,8 @@ class pool_memory_resource_impl final
 
   [[nodiscard]] std::size_t pool_size() const noexcept;
 
-  friend void get_property(pool_memory_resource_impl const&, cuda::mr::device_accessible) noexcept
+  RMM_CONSTEXPR_FRIEND void get_property(pool_memory_resource_impl const&,
+                                         cuda::mr::device_accessible) noexcept
   {
   }
 
--- cpp/include/rmm/mr/pool_memory_resource.hpp
+++ cpp/include/rmm/mr/pool_memory_resource.hpp
@@ -73,7 +73,10 @@ class RMM_EXPORT pool_memory_resource
    *
    * This property declares that a `pool_memory_resource` provides device accessible memory
    */
-  friend void get_property(pool_memory_resource const&, cuda::mr::device_accessible) noexcept {}
+  RMM_CONSTEXPR_FRIEND void get_property(pool_memory_resource const&,
+                                         cuda::mr::device_accessible) noexcept
+  {
+  }
 
   /**
    * @brief Construct a `pool_memory_resource` and allocate the initial device memory pool using
