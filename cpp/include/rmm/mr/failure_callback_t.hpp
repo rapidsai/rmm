cd0b516b0c53cecc4021338e0c542722ae7b7750
Bradley Dice <bdice@bradleydice.com>
2026-03-11 16:41:47 -0500

    Apply suggestions from code review
    Co-authored-by: David Wendt <45795991+davidwendt@users.noreply.github.com>

--- cpp/include/rmm/mr/failure_callback_t.hpp
+++ cpp/include/rmm/mr/failure_callback_t.hpp
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2020-2026, NVIDIA CORPORATION.
+ * SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION.
  * SPDX-License-Identifier: Apache-2.0
  */
 #pragma once
@@ -28,7 +28,7 @@ namespace mr {
  *
  * The callback function is passed two parameters: `bytes` is the size of the failed memory
  * allocation and `arg` is the extra argument passed to the constructor of the
- * `failure_callback_resource_adaptor`. The callback function returns a Boolean where true means to
+ * `failure_callback_resource_adaptor`. The callback function returns a bool where true means to
  * retry the memory allocation and false means to re-throw the exception.
  */
 using failure_callback_t = std::function<bool(std::size_t, void*)>;
