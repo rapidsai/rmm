272bca572d969c5f866399652e4d69395210a496
James Lamb <jlamb@nvidia.com>
2026-03-12 16:16:18 -0500

    use rapids_config instead

--- cpp/examples/versions.cmake
+++ cpp/examples/versions.cmake
@@ -5,11 +5,5 @@
 # cmake-format: on
 # =============================================================================
 
-# Use STRINGS to trim whitespace/newlines
-file(STRINGS "${CMAKE_CURRENT_LIST_DIR}/../../RAPIDS_BRANCH" RMM_TAG)
-if(NOT RMM_TAG)
-  message(
-    FATAL_ERROR
-      "Could not determine branch name to use for checking out rapids-cmake. The file \"${CMAKE_CURRENT_LIST_DIR}/../../RAPIDS_BRANCH\" is missing."
-  )
-endif()
+include(${CMAKE_CURRENT_LIST_DIR}/../../cmake/rapids_config.cmake)
+set(RMM_TAG ${_rapids_branch})
