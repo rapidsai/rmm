0115cf15fbad5d8a3e8d033bc676d955ff0a51cb
James Lamb <jaylamb20@gmail.com>
2026-03-12 17:04:40 -0500

    examples: read tag from RAPIDS_BRANCH file (#2293)
    Fixes these `pre-commit` errors blocking CI:
    
    ```text
    verify-hardcoded-version.................................................Failed
    - hook id: verify-hardcoded-version
    - exit code: 1
    
    In file RAPIDS_BRANCH:1:9:
     release/26.04
    warning: do not hard-code version, read from VERSION file instead
    
    In file RAPIDS_BRANCH:1:9:
     release/26.04
    
    In file cpp/examples/versions.cmake:8:21:
     set(RMM_TAG release/26.04)
    warning: do not hard-code version, read from VERSION file instead
    
    In file cpp/examples/versions.cmake:8:21:
     set(RMM_TAG release/26.04)
    ```
    
    By updating `verify-hardcoded-version` configuration and by updating the C++ examples to read `RMM_TAG` from the `RAPIDS_BRANCH` file.
    
    See https://github.com/rapidsai/pre-commit-hooks/issues/121 for details
    
    Authors:
      - James Lamb (https://github.com/jameslamb)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/rmm/pull/2293

--- cpp/examples/versions.cmake
+++ cpp/examples/versions.cmake
@@ -1,8 +1,9 @@
 # =============================================================================
 # cmake-format: off
-# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 # cmake-format: on
 # =============================================================================
 
-set(RMM_TAG release/26.04)
+include(${CMAKE_CURRENT_LIST_DIR}/../../cmake/rapids_config.cmake)
+set(RMM_TAG ${_rapids_branch})
