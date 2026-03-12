fb8ef5a401c6a0dc844841192a7f3a6431e5c27e
James Lamb <jlamb@nvidia.com>
2026-03-12 15:29:22 -0500

    examples: read tag from RAPIDS_BRANCH file

--- cpp/examples/versions.cmake
+++ cpp/examples/versions.cmake
@@ -1,8 +1,15 @@
 # =============================================================================
 # cmake-format: off
-# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 # cmake-format: on
 # =============================================================================
 
-set(RMM_TAG release/26.04)
+# Use STRINGS to trim whitespace/newlines
+file(STRINGS "${CMAKE_CURRENT_LIST_DIR}/../../RAPIDS_BRANCH" RMM_TAG)
+if(NOT RMM_TAG)
+  message(
+    FATAL_ERROR
+      "Could not determine branch name to use for checking out rapids-cmake. The file \"${CMAKE_CURRENT_LIST_DIR}/../../RAPIDS_BRANCH\" is missing."
+  )
+endif()
