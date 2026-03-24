8a78730a3350e0711a0944fd33f4b336a9522ad4
Lawrence Mitchell <lmitchell@nvidia.com>
2026-03-24 11:44:45 +0000

    Add tests of new alignment-preserving allocation

--- cpp/tests/device_scalar_tests.cpp
+++ cpp/tests/device_scalar_tests.cpp
@@ -3,7 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <rmm/aligned.hpp>
 #include <rmm/cuda_stream.hpp>
+#include <rmm/detail/error.hpp>
 #include <rmm/device_scalar.hpp>
 #include <rmm/mr/device_memory_resource.hpp>
 #include <rmm/mr/per_device_resource.hpp>
@@ -140,4 +142,20 @@ TYPED_TEST(DeviceScalarTest, SetGetStream)
   scalar.set_stream(otherstream);
 
   EXPECT_EQ(scalar.stream(), otherstream);
+}
+
+TEST(DeviceScalarAlignmentTest, SmallAlignment)
+{
+  auto s = rmm::device_scalar<int>(rmm::cuda_stream_view{});
+  EXPECT_TRUE(rmm::is_pointer_aligned(s.data(), std::alignment_of_v<decltype(s)::value_type>));
+}
+
+TEST(DeviceScalarAlignmentTest, LargeAlignment)
+{
+  struct alignas(rmm::CUDA_ALLOCATION_ALIGNMENT * 2) OverAligned {
+    int value;
+  };
+
+  EXPECT_THROW(std::ignore = rmm::device_scalar<OverAligned>(rmm::cuda_stream_view{}),
+               rmm::bad_alloc);
 }
