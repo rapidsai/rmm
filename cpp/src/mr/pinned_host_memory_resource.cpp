65057b82fdf450b79e9aeefa4a8231e8a1e0a5ef
Bradley Dice <bdice@bradleydice.com>
2026-05-27 13:25:26 -0500

    Move public memory resource definitions to source files (#2416)
    Moves several "base" memory resource definitions from headers to corresponding source files under `cpp/src/mr`, matching the style used by other RMM memory resources. These were missed during the earlier migration to CCCL memory resources because the heavier focus was on adaptors.
    
    This also exports the affected public memory resource classes for shared-library builds and adds direct test includes that were previously satisfied transitively by implementation-heavy headers.
    
    Closes #2414
    
    Authors:
      - Bradley Dice (https://github.com/bdice)
    
    Approvers:
      - Lawrence Mitchell (https://github.com/wence-)
      - Kyle Edwards (https://github.com/KyleFromNVIDIA)
    
    URL: https://github.com/rapidsai/rmm/pull/2416

---
+++ cpp/src/mr/pinned_host_memory_resource.cpp
@@ -0,0 +1,72 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2024-2026, NVIDIA CORPORATION.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <rmm/detail/aligned.hpp>
+#include <rmm/detail/error.hpp>
+#include <rmm/mr/pinned_host_memory_resource.hpp>
+
+#include <cuda/stream_ref>
+#include <cuda_runtime_api.h>
+
+#include <cstddef>
+
+namespace RMM_NAMESPACE {
+namespace mr {
+
+void* pinned_host_memory_resource::allocate([[maybe_unused]] cuda::stream_ref stream,
+                                            std::size_t bytes,
+                                            std::size_t alignment)
+{
+  // don't allocate anything if the user requested zero bytes
+  if (0 == bytes) { return nullptr; }
+  RMM_EXPECTS(rmm::is_supported_base_resource_alignment(alignment),
+              "Requested alignment is larger than this memory resource supports.",
+              rmm::bad_alloc);
+
+  std::size_t constexpr alloc_alignment = rmm::CUDA_ALLOCATION_ALIGNMENT;
+  return rmm::detail::aligned_host_allocate(bytes, alloc_alignment, [](std::size_t size) {
+    void* ptr{nullptr};
+    RMM_CUDA_TRY_ALLOC(cudaHostAlloc(&ptr, size, cudaHostAllocDefault), size);
+    return ptr;
+  });
+}
+
+void pinned_host_memory_resource::deallocate([[maybe_unused]] cuda::stream_ref stream,
+                                             void* ptr,
+                                             std::size_t bytes,
+                                             [[maybe_unused]] std::size_t alignment) noexcept
+{
+  std::size_t constexpr alloc_alignment = rmm::CUDA_ALLOCATION_ALIGNMENT;
+  rmm::detail::aligned_host_deallocate(ptr, bytes, alloc_alignment, [](void* ptr) {
+    RMM_ASSERT_CUDA_SUCCESS_SAFE_SHUTDOWN(cudaFreeHost(ptr));
+  });
+}
+
+void* pinned_host_memory_resource::allocate_sync(std::size_t bytes, std::size_t alignment)
+{
+  auto* ptr = allocate(cuda::stream_ref{cudaStream_t{nullptr}}, bytes, alignment);
+  RMM_CUDA_TRY(cudaStreamSynchronize(cudaStream_t{nullptr}));
+  return ptr;
+}
+
+void pinned_host_memory_resource::deallocate_sync(void* ptr,
+                                                  std::size_t bytes,
+                                                  std::size_t alignment) noexcept
+{
+  deallocate(cuda::stream_ref{cudaStream_t{nullptr}}, ptr, bytes, alignment);
+}
+
+bool pinned_host_memory_resource::operator==(pinned_host_memory_resource const&) const noexcept
+{
+  return true;
+}
+
+bool pinned_host_memory_resource::operator!=(pinned_host_memory_resource const&) const noexcept
+{
+  return false;
+}
+
+}  // namespace mr
+}  // namespace RMM_NAMESPACE
