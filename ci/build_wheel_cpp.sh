f24a577db89f9d6e68f68851fb091e1908df46d7
James Lamb <jaylamb20@gmail.com>
2026-08-05 10:39:16 -0500

    ensure nightly builds always produce new packages, expand 'changed-files' lists (#2502)
    Contributes to https://github.com/rapidsai/build-planning/issues/218
    
    * uses the new patterns for versioning nightly packages (see https://github.com/rapidsai/shared-workflows/pull/603), to ensure they're always uploaded on new builds. 
    
    For wheels:
    
    ```text
    # before
    26.10.0a55 
    
    # after
    26.10.0a55.post260803200854
    ```
    
    And for conda:
    
    ```text
    # before
    26.10.0a55[build=cuda12_260803_9da146ef]
    
    # after
    26.10.0a55[build=cuda12_260803200854_9da146ef]
    ```
    
    Other changes:
    
    * updates `changed-files` lists to avoid triggering test jobs in PR CI when only `.github/workflows/{build,test}.yaml` are changed
    
    ## Notes for Reviewers
    
    ### How I tested this
    
    See https://github.com/rapidsai/cugraph-gnn/pull/508
    
    Authors:
      - James Lamb (https://github.com/jameslamb)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/rmm/pull/2502

--- ci/build_wheel_cpp.sh
+++ ci/build_wheel_cpp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: Copyright (c) 2024-2026, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2024-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 set -euo pipefail
@@ -7,13 +7,14 @@ set -euo pipefail
 package_dir="python/librmm"
 
 source rapids-configure-sccache
-source rapids-date-string
+source rapids-datetime-string
 source rapids-init-pip
 
 export SCCACHE_S3_PREPROCESSOR_CACHE_KEY_PREFIX="librmm-${RAPIDS_CONDA_ARCH}-cuda${RAPIDS_CUDA_VERSION%%.*}-wheel-preprocessor-cache"
 export SCCACHE_S3_USE_PREPROCESSOR_CACHE_MODE=true
 
-rapids-generate-version > ./VERSION
+RAPIDS_VERSION_SUFFIX=".post${RAPIDS_DATETIME_STRING}" \
+  rapids-generate-version > ./VERSION
 
 cd "${package_dir}"
 
