b3942819ce802f68725955b2930e32779877da3d
Kyle Edwards <kyedwards@nvidia.com>
2026-09-16 11:36:39 -0400

    Use new `verify-dependencies` hook from pre-commit-hooks
    This hook enforces `dependencies.yaml` conventions, starting with
    ensuring that the correct `cuda_suffixed` and `use_cuda_wheels`
    matrix entries are used.
    
    Issue: https://github.com/rapidsai/pre-commit-hooks/issues/132

--- build.sh
+++ build.sh
@@ -39,6 +39,12 @@ LIBRMM_BUILD_DIR=${LIBRMM_BUILD_DIR:=${REPODIR}/cpp/build}
 RMM_BUILD_DIR="${REPODIR}/python/rmm/build"
 BUILD_DIRS="${LIBRMM_BUILD_DIR} ${RMM_BUILD_DIR}"
 
+CUDA_VERSION="${RAPIDS_CUDA_VERSION:-$(nvcc --version | sed -E -n 's/^.*release ([0-9]+\.[0-9]+).*$/\1/p')}"
+if [[ -z "$CUDA_VERSION" ]]; then
+    echo "Could not determine CUDA version. Please set RAPIDS_CUDA_VERSION or make sure your \$PATH contains a valid nvcc."
+    exit 1
+fi
+
 # Set defaults for vars modified by flags to this script
 VERBOSE_FLAG=""
 BUILD_TYPE=Release
@@ -58,6 +64,7 @@ PYTHON_ARGS_FOR_INSTALL=(
     --no-build-isolation
     --no-deps
     --config-settings="rapidsai.disable-cuda=true"
+    --config-settings="rapidsai.matrix-entry=cuda=${CUDA_VERSION};cuda_suffixed=false;use_cuda_wheels=false"
 )
 
 
